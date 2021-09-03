3a1a5f8bc81fe71ab6d039e47fbe75106eebcea8
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2021-09-03 10:20:41 +0100

    update opentyrian

--- package/opentyrian/opentyrian.mk
+++ package/opentyrian/opentyrian.mk
@@ -3,16 +3,16 @@
 # opentyrian
 #
 ################################################################################
-
-OPENTYRIAN_VERSION = cf5dbeb69eebd9ef9afc4473088d9469b79589eb
+# Version.: Commits on Nov 29, 2020
+OPENTYRIAN_VERSION = 0af1efb81d7d09235ca8d384eee0d8550efc1ff4
 OPENTYRIAN_SITE = $(call github,opentyrian,opentyrian,$(OPENTYRIAN_VERSION))
 OPENTYRIAN_LICENSE = GPL-2.0+
 OPENTYRIAN_LICENSE_FILES = COPYING
 
-OPENTYRIAN_DEPENDENCIES = sdl
+OPENTYRIAN_DEPENDENCIES = sdl2
 
 ifeq ($(BR2_PACKAGE_OPENTYRIAN_NET),y)
-OPENTYRIAN_DEPENDENCIES += sdl_net
+OPENTYRIAN_DEPENDENCIES += sdl2_net
 OPENTYRIAN_NETWORK = true
 else
 OPENTYRIAN_NETWORK = false
@@ -22,10 +22,10 @@ define OPENTYRIAN_BUILD_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) PLATFORM=UNIX \
 		CC="$(TARGET_CC)" \
 		STRIP="/bin/true" \
-		SDL_CONFIG="$(STAGING_DIR)/usr/bin/sdl-config" \
+		SDL_CONFIG="$(STAGING_DIR)/usr/bin/sdl2-config" \
 		LDFLAGS="$(TARGET_LDFLAGS) -lm" \
 		WITH_NETWORK="$(OPENTYRIAN_NETWORK)" \
-		-C $(@D) release
+		-C $(@D)
 endef
 
 define OPENTYRIAN_INSTALL_TARGET_CMDS
