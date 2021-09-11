28a76a68914a5599801cc03008551178b75bd084
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2021-09-11 10:12:43 +0100

    Revert "update rpi4 firmware"
    This reverts commit c53f52cd96188039828b7bb3c68eb36892ba07fb.

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = fc8f0d30a439da71770aae9cd8ac0627087bcb1c
+RPI_FIRMWARE_VERSION = 24f05a6e0eadcf001159e3618759cfb51761fd0e
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
