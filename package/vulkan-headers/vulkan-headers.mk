d97f650dbffd69072d4f11c15e9303dbd0be4c61
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2022-02-08 20:04:48 +0000

    Update vulkan-headers.mk

--- package/vulkan-headers/vulkan-headers.mk
+++ package/vulkan-headers/vulkan-headers.mk
@@ -3,9 +3,9 @@
 # vulkan-headers
 #
 ################################################################################
-
-VULKAN_HEADERS_VERSION = 1.3.204
-VULKAN_HEADERS_SITE = $(call github,KhronosGroup,Vulkan-Headers,v$(VULKAN_HEADERS_VERSION))
+# Version.: Commits on Feb 04, 2022
+VULKAN_HEADERS_VERSION = b32da5329b50e3cb96229aaecba9ded032fe29cc
+VULKAN_HEADERS_SITE = $(call github,KhronosGroup,Vulkan-Headers,$(VULKAN_HEADERS_VERSION))
 VULKAN_HEADERS_LICENSE = Apache-2.0
 VULKAN_HEADERS_LICENSE_FILES = LICENSE.txt
 VULKAN_HEADERS_INSTALL_STAGING = YES
