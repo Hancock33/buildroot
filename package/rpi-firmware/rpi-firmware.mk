368795b4b3c81b42ec262f46352d17777b553c98
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2022-02-09 07:16:30 +0000

    update rpi kernel to  5.15.21

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -3,8 +3,8 @@
 # rpi-firmware
 #
 ################################################################################
-# Version.: Commits on Feb 04, 2022 version 5.15.18
-RPI_FIRMWARE_VERSION = 48606ad5e152227db4a26459fc2ba7944b6597a3
+# Version.: Commits on Feb 07, 2022 version 5.15.18
+RPI_FIRMWARE_VERSION = ad1c9ac829c43addd3da7476eecd8730a0089b32
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
