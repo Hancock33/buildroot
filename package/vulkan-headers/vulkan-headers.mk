f94743db60c897e1de1296519b8fbcf6111dcd06
Lee <lee.hancock4@ntlworld.com>
2022-02-19 12:49:06 +0000

    update vulkan-headers

--- package/vulkan-headers/vulkan-headers.mk
+++ package/vulkan-headers/vulkan-headers.mk
@@ -3,8 +3,8 @@
 # vulkan-headers
 #
 ################################################################################
-# Version.: Commits on Feb 04, 2022
-VULKAN_HEADERS_VERSION = b32da5329b50e3cb96229aaecba9ded032fe29cc
+# Version.: Commits on Feb 18, 2022
+VULKAN_HEADERS_VERSION = aa18f182ebba65438b1cfdbd571f020bb2e34d04
 VULKAN_HEADERS_SITE = $(call github,KhronosGroup,Vulkan-Headers,$(VULKAN_HEADERS_VERSION))
 VULKAN_HEADERS_LICENSE = Apache-2.0
 VULKAN_HEADERS_LICENSE_FILES = LICENSE.txt
