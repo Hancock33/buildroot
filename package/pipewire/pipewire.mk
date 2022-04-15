9ff405da225c6f9bb5b99e6b64d68c3a805dd20a
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2022-04-15 17:22:56 +0100

    Update pipewire.mk
    fixes C90 header compile against SDL2

--- package/pipewire/pipewire.mk
+++ package/pipewire/pipewire.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-# Batocera Update
-PIPEWIRE_VERSION = 0.3.50
+# Batocera Update (0.3.50)
+PIPEWIRE_VERSION = 22fc9eec351a24bfde1b6fbc77927ff087ddd739
 PIPEWIRE_SITE = $(call github,PipeWire,pipewire,$(PIPEWIRE_VERSION))
 PIPEWIRE_LICENSE = MIT, LGPL-2.1+ (libspa-alsa), GPL-2.0 (libjackserver)
 PIPEWIRE_LICENSE_FILES = COPYING LICENSE
