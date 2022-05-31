ea4a8184116d880bf21eb4e17708aab6c8c13771
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2022-05-31 07:13:29 +0100

    update mesa demos

--- package/mesa3d-demos/mesa3d-demos.mk
+++ package/mesa3d-demos/mesa3d-demos.mk
@@ -4,16 +4,16 @@
 #
 ################################################################################
 
-MESA3D_DEMOS_VERSION = 8.4.0
+MESA3D_DEMOS_VERSION = 8.5.0
 MESA3D_DEMOS_SOURCE = mesa-demos-$(MESA3D_DEMOS_VERSION).tar.bz2
-MESA3D_DEMOS_SITE = ftp://ftp.freedesktop.org/pub/mesa/demos
+MESA3D_DEMOS_SITE = https://archive.mesa3d.org/demos/$(MESA3D_DEMOS_VERSION)
 # 0001-demos-makes-opengl-an-optional-component.patch
 MESA3D_DEMOS_AUTORECONF = YES
 MESA3D_DEMOS_DEPENDENCIES = host-pkgconf
 MESA3D_DEMOS_LICENSE = MIT
 
 MESA3D_DEMOS_CONF_OPTS += \
-	--disable-gles1
+	--disable-gles1 --enable-autotools
 
 ifeq ($(BR2_PACKAGE_XORG7)$(BR2_PACKAGE_HAS_LIBGL),yy)
 MESA3D_DEMOS_DEPENDENCIES += libgl libglew libglu xlib_libX11 xlib_libXext
@@ -36,25 +36,11 @@ else
 MESA3D_DEMOS_CONF_OPTS += --disable-gles2
 endif
 
-ifeq ($(BR2_PACKAGE_HAS_LIBOPENVG),y)
-MESA3D_DEMOS_DEPENDENCIES += libopenvg
-MESA3D_DEMOS_CONF_OPTS += --enable-vg
-else
-MESA3D_DEMOS_CONF_OPTS += --disable-vg
-endif
-
 ifeq ($(BR2_PACKAGE_MESA3D_GBM),y)
 MESA3D_DEMOS_DEPENDENCIES += mesa3d
 MESA3D_DEMOS_CONF_OPTS += --enable-gbm
 else
 MESA3D_DEMOS_CONF_OPTS += --disable-gbm
-endif
-
-ifeq ($(BR2_PACKAGE_FREETYPE),y)
-MESA3D_DEMOS_DEPENDENCIES += freetype
-MESA3D_DEMOS_CONF_OPTS += --enable-freetype2
-else
-MESA3D_DEMOS_CONF_OPTS += --disable-freetype2
 endif
 
 ifeq ($(BR2_PACKAGE_LIBFREEGLUT),y)
