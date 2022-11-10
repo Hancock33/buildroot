f8829789903e07f5b51c4723fb8ec72be3755f7f
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2022-11-10 18:47:30 +0000

    update pipewire

--- package/vulkan-headers/vulkan-headers.mk
+++ package/vulkan-headers/vulkan-headers.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version: Commits on Nov 08, 2022
-VULKAN_HEADERS_VERSION = 689821914113a7e50cc8f44913699b4127b88eb3
+VULKAN_HEADERS_VERSION = 29c0457cc167bfc9e9361a3818440e388986f5b5
 VULKAN_HEADERS_SITE = $(call github,KhronosGroup,Vulkan-Headers,$(VULKAN_HEADERS_VERSION))
 VULKAN_HEADERS_LICENSE = Apache-2.0
 VULKAN_HEADERS_LICENSE_FILES = LICENSE.txt
