dfb840f988b3b01ae76c63710cb01582e8877356
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2022-11-28 23:18:54 +0000

    cleanup + update opentyrian

--- package/opentyrian/opentyrian.mk
+++ package/opentyrian/opentyrian.mk
@@ -3,8 +3,8 @@
 # opentyrian
 #
 ################################################################################
-# Version: Commits on Oct 16, 2022
-OPENTYRIAN_VERSION = e60885b7069d6723b103abbb2e3f09c227fc8d86
+# Version: Commits on Oct 21, 2022
+OPENTYRIAN_VERSION = 0ac428c4a78f322a9f253415b2c481e3cd139285
 OPENTYRIAN_SITE = $(call github,opentyrian,opentyrian,$(OPENTYRIAN_VERSION))
 OPENTYRIAN_LICENSE = GPL-2.0+
 OPENTYRIAN_LICENSE_FILES = COPYING
@@ -30,12 +30,6 @@ endef
 
 define OPENTYRIAN_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/opentyrian $(TARGET_DIR)/usr/bin/opentyrian
-	# install media
-    mkdir -p $(TARGET_DIR)/usr/share/emulationstation/ports/opentyrian
-	cp -a  $(BR2_EXTERNAL_BATOCERA_PATH)/buildroot/package/opentyrian/media/* $(TARGET_DIR)/usr/share/emulationstation/ports/opentyrian/
-    # evmap config
-	mkdir -p $(TARGET_DIR)/usr/share/evmapy
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/buildroot/package/opentyrian/opentyrian.keys $(TARGET_DIR)/usr/share/evmapy
 endef
 
 $(eval $(generic-package))
