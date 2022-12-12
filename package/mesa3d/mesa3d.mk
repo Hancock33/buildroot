cd59a398f6fa40ce221b996016f057468ac3907a
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2022-12-12 20:46:03 +0000

    Update mesa3d.mk

--- package/mesa3d/mesa3d.mk
+++ package/mesa3d/mesa3d.mk
@@ -3,9 +3,9 @@
 # mesa3d
 #
 ################################################################################
-# Version: Commits on Dec 09, 2022 (22.3.0)
+# Version: Commits on Dec 12, 2022 (22.3.0)
 # When updating the version, please also update mesa3d-headers
-MESA3D_VERSION = 02d23a79bec6dd80ade097d079b7057e46dec864
+MESA3D_VERSION = ad2ec395acf8acba5c8b40f6ba5db5a7baf8b2de
 MESA3D_SITE = $(call github,Mesa3D,mesa,$(MESA3D_VERSION))
 MESA3D_LICENSE = MIT, SGI, Khronos
 MESA3D_LICENSE_FILES = docs/license.rst
