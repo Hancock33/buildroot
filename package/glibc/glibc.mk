62ec3b3fe6d036c319b45c695c52b5883298f2df
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-02-03 23:02:04 +0000

    update build tools

--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -7,7 +7,7 @@
 # Generate version string using:
 #   git describe --match 'glibc-*' --abbrev=40 origin/release/MAJOR.MINOR/master | cut -d '-' -f 2-
 # When updating the version, please also update localedef
-GLIBC_VERSION = 2.36-84-g93967a2a7bbdcedb73e0b246713580c7c84d001e
+GLIBC_VERSION = 2.37-1-g020b43544a26237d752eae4a715c403f29226542
 # Upstream doesn't officially provide an https download link.
 # There is one (https://sourceware.org/git/glibc.git) but it's not reliable,
 # sometimes the connection times out. So use an unofficial github mirror.
