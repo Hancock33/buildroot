d60b23aa8278843927e44f73816d0d0606492afb
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-02-11 19:34:54 +0000

    update glibc + gcc

--- package/gcc/gcc.mk
+++ package/gcc/gcc.mk
@@ -15,7 +15,7 @@ GCC_SITE = $(call github,foss-for-synopsys-dwc-arc-processors,gcc,$(GCC_VERSION)
 GCC_SOURCE = gcc-$(GCC_VERSION).tar.gz
 else ifeq ($(BR2_GCC_VERSION_12_GIT),y)
 #git describe --abbrev=40 origin/releases/gcc-12 | cut -d '-' -f 2-
-GCC_VERSION =  12.2.0-418-g6bd330777a36a1d23ca2f3ebc99a1bec0fa1b1a4
+GCC_VERSION =  12.2.0-468-gabaa8f9cc4c06dcdae13a33be9253762861d8657
 GCC_SITE = $(call github,RTEMS,gnu-mirror-gcc,$(GCC_VERSION))
 GCC_SOURCE = gcc-$(GCC_VERSION).tar.gz
 else
--- package/glibc/glibc.mk
+++ package/glibc/glibc.mk
@@ -7,7 +7,7 @@
 # Generate version string using:
 #   git describe --match 'glibc-*' --abbrev=40 origin/release/MAJOR.MINOR/master | cut -d '-' -f 2-
 # When updating the version, please also update localedef
-GLIBC_VERSION = 2.37-3-gfb7b95dc47b0d5ecd48339512d091b6b23e7d900
+GLIBC_VERSION = 2.37-6-gd8e1a7590d375159fb5aac07ad8111ab4699e994
 # Upstream doesn't officially provide an https download link.
 # There is one (https://sourceware.org/git/glibc.git) but it's not reliable,
 # sometimes the connection times out. So use an unofficial github mirror.
