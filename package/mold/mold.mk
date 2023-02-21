6ffb3afe585a98c4105bb0f04175c5af8513bfee
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-02-21 20:21:44 +0000

    Update mold.mk

--- package/mold/mold.mk
+++ package/mold/mold.mk
@@ -3,8 +3,8 @@
 # mold
 #
 ################################################################################
-# Version: Commits on Feb 19, 2023
-MOLD_VERSION = e1a7590db372e3ae0c1e0b18c6dfa5e0165ed85f
+# Version: Commits on Feb 21, 2023
+MOLD_VERSION = 5ae8ec966963b1725dee3178e42a4c5d54babc59
 MOLD_SITE = $(call github,rui314,mold,$(MOLD_VERSION))
 MOLD_LICENSE = GPLv3
 MOLD_DEPENDENCIES = zlib $(TARGET_NLS_DEPENDENCIES)
@@ -12,7 +12,7 @@ HOST_MOLD_DEPENDENCIES += host-flex host-bison host-openssl host-zstd
 
 MOLD_SUPPORTS_IN_SOURCE_BUILD = NO
 MOLD_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release -DMOLD_LTO=ON
-HOST_MOLD_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release -DMOLD_LTO=ON
+HOST_MOLD_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
 
 define MOLD_INSTALL
     rm -rf $(HOST_DIR)/bin/mold.ld
