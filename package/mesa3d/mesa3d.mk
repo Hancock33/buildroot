d6edafba3e6ad52e122699765d0dcde6812935d5
Lee <lee.hancock4@ntlworld.com>
2023-03-23 23:34:34 +0000

    mesa from git

--- package/mesa3d/mesa3d.mk
+++ package/mesa3d/mesa3d.mk
@@ -5,9 +5,9 @@
 ################################################################################
 
 # When updating the version, please also update mesa3d-headers
-MESA3D_VERSION = 23.0.0
-MESA3D_SOURCE = mesa-$(MESA3D_VERSION).tar.xz
-MESA3D_SITE = https://archive.mesa3d.org
+MESA3D_VERSION = 032a428fac08f67828d2939f72073ed27b7bae46
+MESA3D_SITE = https://gitlab.freedesktop.org/mesa/mesa.git
+MESA3D_SITE_METHOD = git
 MESA3D_LICENSE = MIT, SGI, Khronos
 MESA3D_LICENSE_FILES = docs/license.rst
 MESA3D_CPE_ID_VENDOR = mesa3d
