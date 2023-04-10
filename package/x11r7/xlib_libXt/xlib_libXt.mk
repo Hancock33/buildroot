41795eb7cff987610bb07c9f09e0f489e760b249
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-04-10 13:09:40 +0100

    update libxt

--- package/x11r7/xlib_libXt/xlib_libXt.mk
+++ package/x11r7/xlib_libXt/xlib_libXt.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-XLIB_LIBXT_VERSION = 1.2.1
-XLIB_LIBXT_SOURCE = libXt-$(XLIB_LIBXT_VERSION).tar.bz2
+XLIB_LIBXT_VERSION = 1.3.0
+XLIB_LIBXT_SOURCE = libXt-$(XLIB_LIBXT_VERSION).tar.xz
 XLIB_LIBXT_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBXT_LICENSE = MIT
 XLIB_LIBXT_LICENSE_FILES = COPYING
