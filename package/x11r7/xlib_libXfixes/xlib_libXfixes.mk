8652180ef184e9f93ceb39500781f8cfceec5a81
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-04-10 13:03:11 +0100

    update libXfixes

--- package/x11r7/xlib_libXfixes/xlib_libXfixes.mk
+++ package/x11r7/xlib_libXfixes/xlib_libXfixes.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-XLIB_LIBXFIXES_VERSION = 6.0.0
-XLIB_LIBXFIXES_SOURCE = libXfixes-$(XLIB_LIBXFIXES_VERSION).tar.bz2
+XLIB_LIBXFIXES_VERSION = 6.0.1
+XLIB_LIBXFIXES_SOURCE = libXfixes-$(XLIB_LIBXFIXES_VERSION).tar.xz
 XLIB_LIBXFIXES_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBXFIXES_LICENSE = MIT
 XLIB_LIBXFIXES_LICENSE_FILES = COPYING
