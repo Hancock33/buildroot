6349f24277451a06e9221d451ad1231d403a190d
Lee <lee.hancock4@ntlworld.com>
2023-04-16 08:15:12 +0100

    incorrect mesa3d tag

--- package/mesa3d/mesa3d.mk
+++ package/mesa3d/mesa3d.mk
@@ -5,7 +5,7 @@
 ################################################################################
 # When updating the version, please also update mesa3d-headers
 # git describe --abbrev=40 origin/staging/23.1 | cut -d '-' -f 1-
-MESA3D_VERSION = 23.1.0-rc1
+MESA3D_VERSION = mesa-23.1.0-rc1
 MESA3D_SITE = $(call github,sailfishos-mirror,mesa,$(MESA3D_VERSION))
 MESA3D_LICENSE = MIT, SGI, Khronos
 MESA3D_LICENSE_FILES = docs/license.rst
