d105bdf40005edd6a94b349063b127af1686f815
Lee <lee.hancock4@ntlworld.com>
2023-04-21 19:56:53 +0100

    update mesa

--- package/mesa3d/mesa3d.mk
+++ package/mesa3d/mesa3d.mk
@@ -4,12 +4,9 @@
 #
 ################################################################################
 # When updating the version, please also update mesa3d-headers
-MESA3D_VERSION = 23.1.0-rc2
-MESA3D_SOURCE = mesa-$(MESA3D_VERSION).tar.xz
-MESA3D_SITE = https://archive.mesa3d.org
-# git describe --abbrev=40 origin/staging/23.1 | cut -d '-' -f 1-
-#MESA3D_VERSION = 23.1.0-rc1-30-g9bfd616f1980a489e2aa3223a8040a2dff5efc01
-#MESA3D_SITE = $(call github,sailfishos-mirror,mesa,$(MESA3D_VERSION))
+# git describe --abbrev=40 origin/staging/23.1 | cut -d '-' -f 2-
+MESA3D_VERSION = 23.1.0-rc2-22-g0a544636d8d760680701120d783fbdf260d8af84
+MESA3D_SITE = $(call github,sailfishos-mirror,mesa,$(MESA3D_VERSION))
 MESA3D_LICENSE = MIT, SGI, Khronos
 MESA3D_LICENSE_FILES = docs/license.rst
 MESA3D_CPE_ID_VENDOR = mesa3d
