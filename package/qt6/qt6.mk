cb1c850d41082251096d5bb05573433f0f1a85ff
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-05-12 07:28:14 +0100

    tweak qt6

--- package/qt6/qt6.mk
+++ package/qt6/qt6.mk
@@ -12,6 +12,11 @@ QT6_SITE = https://download.qt.io/archive/qt/$(QT6_VERSION_MAJOR)/$(QT6_VERSION)
 QT6_COMMON_CONF_OPTS = \
     -DINSTALL_ARCHDATADIR=/usr/lib/qt6 \
     -DINSTALL_DATADIR=/usr/share/qt6 \
-    -DINSTALL_MKSPECSDIR=/usr/lib/qt6/mkspecs
+    -DINSTALL_MKSPECSDIR=/usr/lib/qt6/mkspecs \
+    -DBUILD_WITH_PCH=OFF \
+    -DQT_BUILD_EXAMPLES=OFF \
+    -DQT_BUILD_TESTS=OFF
+    -DQT_BUILD_TESTS_BY_DEFAULT=OFF \
+    -DQT_BUILD_EXAMPLES_BY_DEFAULT=OFF
 
 include $(sort $(wildcard package/qt6/*/*.mk))
