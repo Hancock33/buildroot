dca9492843ba2bdd82814414112a9805f394d4ae
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-06-29 06:12:16 +0100

    Update mesa3d.mk

--- package/mesa3d/mesa3d.mk
+++ package/mesa3d/mesa3d.mk
@@ -303,7 +303,8 @@ endif
 
 # batocera icd.@0@.json vulkan files
 define MESA3D_VULKANJSON_X86_64
-        $(SED) s+"host_machine.cpu()"+"'x86_64'"+ $(@D)/src/intel/vulkan/meson.build $(@D)/src/amd/vulkan/meson.build
+        $(SED) s+"host_machine.cpu()"+"'x86_64'"+ $(@D)/src/intel/vulkan/meson.build \
+		    $(@D)/src/intel/vulkan_hasvk/meson.build $(@D)/src/amd/vulkan/meson.build
 endef
 
 define MESA3D_VULKANJSON_X86
