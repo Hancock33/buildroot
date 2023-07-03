aa62edad9a83364d0a8a236b0cb1b5cb94e3edd3
Lee <lee.hancock4@ntlworld.com>
2023-07-03 19:53:36 +0100

    [v2,1/1] package/{gettext-gnu, libtextstyle}: bump to version 0.22

--- package/gettext-gnu/gettext-gnu.mk
+++ package/gettext-gnu/gettext-gnu.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-# Please keep in sync with package/libtextstyle/libtextstyle.mk
-GETTEXT_GNU_VERSION = 0.20.1
+# Please keep in sync with package/libtextstyle/libtextstyle.mk and
+# GETTEXT_TINY_ARCHIVE_VERSION in gettext-tiny/gettext-tiny.mk
+GETTEXT_GNU_VERSION = 0.22
 GETTEXT_GNU_SITE = $(BR2_GNU_MIRROR)/gettext
 GETTEXT_GNU_SOURCE = gettext-$(GETTEXT_GNU_VERSION).tar.xz
 GETTEXT_GNU_INSTALL_STAGING = YES
@@ -13,8 +14,6 @@ GETTEXT_GNU_LICENSE = LGPL-2.1+ (libintl), GPL-3.0+ (the rest)
 GETTEXT_GNU_LICENSE_FILES = COPYING gettext-runtime/intl/COPYING.LIB
 GETTEXT_GNU_CPE_ID_VENDOR = gnu
 GETTEXT_GNU_CPE_ID_PRODUCT = gettext
-# 0002-restore-the-ability-to-buld-gettext-tools-seperately-part1.patch
-GETTEXT_GNU_AUTORECONF = YES
 GETTEXT_GNU_PROVIDES = gettext
 GETTEXT_GNU_DEPENDENCIES = $(if $(BR2_PACKAGE_LIBICONV),libiconv)
 
