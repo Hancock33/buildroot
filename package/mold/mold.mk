b586dd941eb4ee5b7a034b06f829af539606945b
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-07-26 18:35:39 +0100

    Update mold.mk

--- package/mold/mold.mk
+++ package/mold/mold.mk
@@ -3,8 +3,8 @@
 # mold
 #
 ################################################################################
-# Version: Commits on Jul 07, 2023
-MOLD_VERSION = 29dc0cf53f7c72d3090adcfb46c4383960c7732a
+# Version: Commits on Jul 26, 2023
+MOLD_VERSION = v2.0.0
 MOLD_SITE = $(call github,rui314,mold,$(MOLD_VERSION))
 MOLD_LICENSE = GPLv3
 MOLD_DEPENDENCIES = zlib $(TARGET_NLS_DEPENDENCIES)
@@ -17,9 +17,9 @@ HOST_MOLD_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
 define MOLD_INSTALL
     rm -rf $(HOST_DIR)/bin/mold.ld
     rm -rf $(HOST_DIR)/bin/ld.mold
- 	cp -av $(HOST_DIR)/bin/mold                       $(HOST_DIR)/$(GNU_TARGET_NAME)/bin/ld.mold
- 	mv     $(HOST_DIR)/bin/mold                       $(HOST_DIR)/bin/$(GNU_TARGET_NAME)-ld.mold
-	ln -sf $(HOST_DIR)/bin/$(GNU_TARGET_NAME)-ld.mold $(HOST_DIR)/bin/$(call qstrip,$(BR2_ARCH))-linux-ld.mold
+    cp -av $(HOST_DIR)/bin/mold                       $(HOST_DIR)/$(GNU_TARGET_NAME)/bin/ld.mold
+    mv     $(HOST_DIR)/bin/mold                       $(HOST_DIR)/bin/$(GNU_TARGET_NAME)-ld.mold
+    ln -sf $(HOST_DIR)/bin/$(GNU_TARGET_NAME)-ld.mold $(HOST_DIR)/bin/$(call qstrip,$(BR2_ARCH))-linux-ld.mold
 endef
 
 HOST_MOLD_POST_INSTALL_HOOKS += MOLD_INSTALL
