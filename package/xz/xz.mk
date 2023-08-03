d1299fb0541afe167a1fb633356f2c3b32019a3f
Lee <lee.hancock4@ntlworld.com>
2023-08-03 01:32:39 +0100

    update xz

--- package/xz/xz.mk
+++ package/xz/xz.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 XZ_VERSION = 5.4.4
-XZ_SOURCE = xz-$(XZ_VERSION).tar.bz2
+XZ_SOURCE = xz-$(XZ_VERSION).tar.xz
 XZ_SITE = https://tukaani.org/xz
 XZ_INSTALL_STAGING = YES
 XZ_CONF_ENV = ac_cv_prog_cc_c99='-std=gnu99'
