b8c76b2c6738df516d44d87038c48c15c67fcdf8
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-08-08 18:32:33 +0100

    Update llvm-project.mk

--- package/llvm-project/llvm-project.mk
+++ package/llvm-project/llvm-project.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LLVM_PROJECT_VERSION_MAJOR = 17
-LLVM_PROJECT_VERSION = $(LLVM_PROJECT_VERSION_MAJOR).0.0rc1
+LLVM_PROJECT_VERSION = $(LLVM_PROJECT_VERSION_MAJOR).0.0rc2
 LLVM_PROJECT_SITE = https://github.com/llvm/llvm-project/releases/download/llvmorg-$(subst rc,-rc,$(LLVM_PROJECT_VERSION))
 
 include $(sort $(wildcard package/llvm-project/*/*.mk))
