16ebf209b141acee31aa90db5eaa36760f75ca62
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-08-16 07:15:16 +0100

    update hidapi

--- package/hidapi/hidapi.mk
+++ package/hidapi/hidapi.mk
@@ -4,13 +4,14 @@
 #
 ################################################################################
 
-HIDAPI_VERSION = 0.11.0
+HIDAPI_VERSION = 0.14.0
 HIDAPI_SITE = $(call github,libusb,hidapi,hidapi-$(HIDAPI_VERSION))
 HIDAPI_INSTALL_STAGING = YES
 # No configure provided, so we need to autoreconf.
 HIDAPI_AUTORECONF = YES
 HIDAPI_LICENSE = GPL-3.0 or BSD-3-Clause or HIDAPI license
 HIDAPI_LICENSE_FILES = LICENSE.txt LICENSE-gpl3.txt LICENSE-bsd.txt LICENSE-orig.txt
+HIDAPI_COMPAT_INSTALL_STAGING = YES
 
 HIDAPI_DEPENDENCIES = libusb libgudev
 
