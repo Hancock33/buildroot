5c100ecb6a9e06de65d7919a9891277eede34da8
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-08-18 23:14:24 +0100

    update sysvinit

--- package/sysvinit/sysvinit.mk
+++ package/sysvinit/sysvinit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SYSVINIT_VERSION = 3.06
+SYSVINIT_VERSION = 3.08
 SYSVINIT_SOURCE = sysvinit-$(SYSVINIT_VERSION).tar.xz
 SYSVINIT_SITE = https://github.com/slicer69/sysvinit/releases/download/$(SYSVINIT_VERSION)
 SYSVINIT_LICENSE = GPL-2.0+
