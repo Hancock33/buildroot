bc92dbbc87ec58bed298df8bb8c1e3f4ab39afce
Lee <lee.hancock4@ntlworld.com>
2023-08-20 10:13:36 +0100

    copy mold shared libs

--- package/mold/mold.mk
+++ package/mold/mold.mk
@@ -13,14 +13,15 @@ HOST_MOLD_DEPENDENCIES += host-flex host-bison host-openssl host-zstd host-cmake
 MOLD_SUPPORTS_IN_SOURCE_BUILD = NO
 MOLD_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release -DMOLD_USE_SYSTEM_TBB=ON
 HOST_MOLD_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release -DMOLD_USE_SYSTEM_TBB=ON
-HOST_MOLD_CONF_OPTS += -DCMAKE_ASM_COMPILER="gcc"
+HOST_MOLD_CONF_OPTS += -DCMAKE_ASM_COMPILER="gcc" -DMOLD_MOSTLY_STATIC=ON
 
 define MOLD_INSTALL
     rm -rf $(HOST_DIR)/bin/mold.ld
     rm -rf $(HOST_DIR)/bin/ld.mold
-    cp -av $(HOST_DIR)/bin/mold                       $(HOST_DIR)/$(GNU_TARGET_NAME)/bin/ld.mold
-    mv     $(HOST_DIR)/bin/mold                       $(HOST_DIR)/bin/$(GNU_TARGET_NAME)-ld.mold
-    ln -sf $(HOST_DIR)/bin/$(GNU_TARGET_NAME)-ld.mold $(HOST_DIR)/bin/$(call qstrip,$(BR2_ARCH))-linux-ld.mold
+    cp -av $(HOST_DIR)/bin/mold                                      $(HOST_DIR)/$(GNU_TARGET_NAME)/bin/ld.mold
+    mv     $(HOST_DIR)/bin/mold                                      $(HOST_DIR)/bin/$(GNU_TARGET_NAME)-ld.mold
+    ln -sf $(HOST_DIR)/bin/$(GNU_TARGET_NAME)-ld.mold                $(HOST_DIR)/bin/$(call qstrip,$(BR2_ARCH))-linux-ld.mold
+    cp -av $(@D)//buildroot-build/third-party/blake3/c/libblake3.so* $(HOST_DIR)/lib
 endef
 
 HOST_MOLD_POST_INSTALL_HOOKS += MOLD_INSTALL
