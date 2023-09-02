52bdb45c421694347b962bc98342d2f00b44f7bc
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-09-02 12:05:31 +0100

    there is some customisation for vulkan in batocera,
    continue to use batocera packages

--- package/vulkan-headers/vulkan-headers.mk
+++ package/vulkan-headers/vulkan-headers.mk
@@ -3,11 +3,9 @@
 # vulkan-headers
 #
 ################################################################################
-
-# Note: changing this version will also affect vulkan-loader and
-# vulkan-tools, so please update those packages as well.
-VULKAN_HEADERS_VERSION = 1.3.262
-VULKAN_HEADERS_SITE = $(call github,KhronosGroup,Vulkan-Headers,v$(VULKAN_HEADERS_VERSION))
+# Version: Commits on Aug 28, 2023
+VULKAN_HEADERS_VERSION = a0c76b4ef76e219483755ff61dce6b67ff79f24b
+VULKAN_HEADERS_SITE = $(call github,KhronosGroup,Vulkan-Headers,$(VULKAN_HEADERS_VERSION))
 VULKAN_HEADERS_LICENSE = Apache-2.0, MIT
 VULKAN_HEADERS_LICENSE_FILES = LICENSE.md
 VULKAN_HEADERS_INSTALL_STAGING = YES
