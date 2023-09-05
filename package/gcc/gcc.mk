613c59fe2f00ea26f26d3498fccc84cf46083956
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-09-05 19:42:52 +0100

    update build tools

--- package/gcc/gcc.mk
+++ package/gcc/gcc.mk
@@ -13,7 +13,7 @@ ifeq ($(BR2_GCC_VERSION_ARC),y)
 	GCC_SOURCE = gcc-$(GCC_VERSION).tar.gz
 else ifeq ($(BR2_GCC_VERSION_13_GIT),y)
 # git describe --abbrev=40 origin/releases/gcc-13 | cut -d '-' -f 2-
-	GCC_VERSION = 13.2.0-149-gbb791011b39813bc7b6fdd0d9831247ace199615
+	GCC_VERSION = 13.2.0-168-g08549dc1b09f6c433eb281e5d88b5b9f8134efee
 	GCC_SITE = $(call github,gcc-mirror,gcc,$(GCC_VERSION))
 	GCC_SOURCE = gcc-$(GCC_VERSION).tar.gz
 else ifeq ($(BR2_GCC_VERSION_12_GIT),y)
