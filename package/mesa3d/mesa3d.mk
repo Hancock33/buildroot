ff1955cc5691031ced5cf13688d1d875e5258939
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-10-07 13:57:02 +0100

    bump mesa3d

--- package/mesa3d/mesa3d.mk
+++ package/mesa3d/mesa3d.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # git describe --abbrev=40 origin/staging/23.1 | cut -d '-' -f 2-
-MESA3D_VERSION = 23.1.9-1-g52ab5584b870be041b93f702693ea3f5665df860
+MESA3D_VERSION = 23.2.1-39-g24e51fa0ff5c2840b83ec4baf5cffc4d595c29c9
 MESA3D_SITE = $(call github,chaotic-cx,mesa-mirror,$(MESA3D_VERSION))
 MESA3D_LICENSE = MIT, SGI, Khronos
 MESA3D_LICENSE_FILES = docs/license.rst
