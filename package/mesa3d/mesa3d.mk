e4927749e7730a5619bc8179e6f0d0145291c498
Lee <lee.hancock4@ntlworld.com>
2023-10-27 22:36:35 +0100

    update mesa-23.3.0-rc1

--- package/mesa3d/mesa3d.mk
+++ package/mesa3d/mesa3d.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # git describe --abbrev=40 origin/staging/23.1 | cut -d '-' -f 2-
-MESA3D_VERSION = 23.3.0-rc1
+MESA3D_VERSION = mesa-23.3.0-rc1
 MESA3D_SITE = $(call github,chaotic-cx,mesa-mirror,$(MESA3D_VERSION))
 MESA3D_LICENSE = MIT, SGI, Khronos
 MESA3D_LICENSE_FILES = docs/license.rst
