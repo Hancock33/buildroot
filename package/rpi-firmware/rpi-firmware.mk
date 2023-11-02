b0118677a1e80b39ba45785291234ab487e9a2aa
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-11-02 19:50:13 +0000

    bump pipewire

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -3,8 +3,8 @@
 # rpi-firmware
 #
 ################################################################################
-# Version: Commits on Oct 30, 2023
-RPI_FIRMWARE_VERSION = c2743f561db19ccc4923eaf715d81db3d15f5c1c
+# Version: Commits on Nov 02, 2023
+RPI_FIRMWARE_VERSION = 83dafbc92c0e63f76ca2ecdd42462d56489d1c77
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
