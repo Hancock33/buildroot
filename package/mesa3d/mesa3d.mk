a168f29ec62b8693472f97dcfdb9604ac8e4853f
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-11-02 19:53:38 +0000

    Update mesa3d.mk

--- package/mesa3d/mesa3d.mk
+++ package/mesa3d/mesa3d.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # git describe --abbrev=40 origin/staging/23.3 | cut -d '-' -f 2-
-MESA3D_VERSION = 23.3.0-rc1-46-g69e6b0307bf9a8a69168d1169ecdfcdb48c6bb84
+MESA3D_VERSION = mesa-23.3.0-rc2
 MESA3D_SITE = $(call github,chaotic-cx,mesa-mirror,$(MESA3D_VERSION))
 MESA3D_LICENSE = MIT, SGI, Khronos
 MESA3D_LICENSE_FILES = docs/license.rst
