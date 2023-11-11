a758c517de45e9da2521dbeb8f9fe711f5b1041d
Lee <lee.hancock4@ntlworld.com>
2023-11-11 12:05:59 +0000

    temp mesa3d workaround

--- package/mesa3d/mesa3d.mk
+++ package/mesa3d/mesa3d.mk
@@ -4,7 +4,12 @@
 #
 ################################################################################
 # git describe --abbrev=40 origin/staging/23.3 | cut -d '-' -f 2-
+ifeq ($(BR2_PACKAGE_MESA3D_LLVM),y)
+MESA3D_VERSION = mesa-23.1.9
+else
 MESA3D_VERSION = 23.3.0-rc3-6-g81ab4a3a01536495329d38d060734fc00ade77ea
+endif
+
 MESA3D_SITE = $(call github,chaotic-cx,mesa-mirror,$(MESA3D_VERSION))
 MESA3D_LICENSE = MIT, SGI, Khronos
 MESA3D_LICENSE_FILES = docs/license.rst
