2a3b35c6927c905de84135776d5a295d18aa6f34
Lee <lee.hancock4@ntlworld.com>
2023-12-02 07:21:12 +0000

    bump enet for dolphin-emu

--- package/enet/enet.mk
+++ package/enet/enet.mk
@@ -3,11 +3,12 @@
 # enet
 #
 ################################################################################
-
-ENET_VERSION = 1.3.17
-ENET_SITE = http://enet.bespin.org/download
+# Version: Commits on Jul 24, 2023
+ENET_VERSION = 2a85cd64459f6ba038d233a634d9440490dbba12
+ENET_SITE = $(call github,lsalzman,enet,$(ENET_VERSION))
 ENET_LICENSE = MIT
 ENET_LICENSE_FILES = LICENSE
+ENET_AUTORECONF = YES
 
 ENET_INSTALL_STAGING = YES
 
