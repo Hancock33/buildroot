3ec3dfdb52cf9a165ba3fe43a82897c295b2020d
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2023-12-15 07:13:09 +0000

    bump btrfs-progs 6.6.3

--- package/btrfs-progs/btrfs-progs.mk
+++ package/btrfs-progs/btrfs-progs.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 # batocera (update)
-BTRFS_PROGS_VERSION = 5.18
+BTRFS_PROGS_VERSION = 6.6.3
 BTRFS_PROGS_SITE = $(BR2_KERNEL_MIRROR)/linux/kernel/people/kdave/btrfs-progs
 BTRFS_PROGS_SOURCE = btrfs-progs-v$(BTRFS_PROGS_VERSION).tar.xz
 BTRFS_PROGS_DEPENDENCIES = host-pkgconf lzo util-linux zlib
