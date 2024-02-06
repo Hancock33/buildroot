bd5ca8fd92afcabb2bc158c3d33a74842b7a3835
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-02-06 18:24:31 +0000

    update rtl8188eu

--- package/rtl8188eu/rtl8188eu.mk
+++ package/rtl8188eu/rtl8188eu.mk
@@ -3,8 +3,8 @@
 # rtl8188eu
 #
 ################################################################################
-# Version: Commits on Dec 04, 2023 (branch@v5.2.2.4)
-RTL8188EU_VERSION = 306f3e62cf7cddf8cee74f70f88a7a7b3af46f56
+# Version: Commits on Feb 06, 2024 (branch@v5.2.2.4)
+RTL8188EU_VERSION = f42fc9c45d2086c415dce70d3018031b54a7beef
 RTL8188EU_SITE = $(call github,lwfinger,rtl8188eu,$(RTL8188EU_VERSION))
 RTL8188EU_LICENSE = GPL-2.0, proprietary (rtl8188eufw.bin firmware blob)
 RTL8188EU_LICENSE_FILES = COPYING
