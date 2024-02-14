d2f2312cbaac6f6e75a67168c2e417e117f452f6
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-02-14 06:40:30 +0000

    Update rpi-firmware.mk

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -3,8 +3,8 @@
 # rpi-firmware
 #
 ################################################################################
-# Version: Commits on Feb 08, 2024
-RPI_FIRMWARE_VERSION = 2a1e5bfd933d977a57ba4cd62f3381fdc339e29a
+# Version: Commits on Feb 13, 2024
+RPI_FIRMWARE_VERSION = a43df3a002f60c4c2243a416d045eb5937585e8b
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
