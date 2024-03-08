9b89a5c6aaca523139d76095ac2db5c49297b354
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-03-08 18:38:35 +0000

    Revert "package/xz: convert to cmake build"
    This reverts commit da068e2077ce5641607e2320eda0b5120e7bfc57.

--- package/xz/xz.mk
+++ package/xz/xz.mk
@@ -8,42 +8,15 @@ XZ_VERSION = 5.6.0
 XZ_SOURCE = xz-$(XZ_VERSION).tar.bz2
 XZ_SITE = https://github.com/tukaani-project/xz/releases/download/v$(XZ_VERSION)
 XZ_INSTALL_STAGING = YES
-XZ_LICENSE = Public Domain, BSD-0-Clause, GPL-2.0+, GPL-3.0+, LGPL-2.1+
-XZ_LICENSE_FILES = COPYING COPYING.0BSD COPYING.GPLv2 COPYING.GPLv3 COPYING.LGPLv2.1
+XZ_CONF_ENV = ac_cv_prog_cc_c99='-std=gnu99'
+XZ_LICENSE = Public Domain, GPL-2.0+, GPL-3.0+, LGPL-2.1+
+XZ_LICENSE_FILES = COPYING COPYING.GPLv2 COPYING.GPLv3 COPYING.LGPLv2.1
 XZ_CPE_ID_VENDOR = tukaani
 
-XZ_CONF_OPTS = \
-	-DENCODERS="lzma1;lzma2;delta;x86;powerpc;ia64;arm;armthumb;arm64;sparc;riscv" \
-	-DDECODERS="lzma1;lzma2;delta;x86;powerpc;ia64;arm;armthumb;arm64;sparc;riscv" \
-	-DMATCH_FINDERS="hc3;hc4;bt2;bt3;bt4" \
-	-DADDITIONAL_CHECK_TYPES="crc64;sha256" \
-	-DMICROLZMA_ENCODER=ON \
-	-DMICROLZMA_DECODER=ON \
-	-DLZIP_DECODER=ON \
-	-DALLOW_CLMUL_CRC=ON \
-	-DALLOW_ARM64_CRC32=ON \
-	-DENABLE_SMALL=OFF \
-	-DENABLE_SANDBOX=ON \
-	-DUSE_ATTR_IFUNC=ON \
-	-DCREATE_XZ_SYMLINKS=ON \
-	-DCREATE_LZMA_SYMLINKS=ON \
-	-DBUILD_SHARED_LIBS=OFF
-
-HOST_XZ_CONF_OPTS = \
-	$(XZ_CONF_OPTS) \
-	-DENABLE_NLS=ON \
-	-DENABLE_THREADS=ON
-
-ifeq ($(BR2_SYSTEM_ENABLE_NLS),y)
-	XZ_CONF_OPTS += -DENABLE_NLS=ON
-else
-	XZ_CONF_OPTS += -DENABLE_NLS=OFF
-endif
-
 ifeq ($(BR2_TOOLCHAIN_HAS_THREADS),y)
-	XZ_CONF_OPTS += -DENABLE_THREADS=ON
+XZ_CONF_OPTS += --enable-threads
 else
-	XZ_CONF_OPTS += -DENABLE_THREADS=OFF
+XZ_CONF_OPTS += --disable-threads
 endif
 
 # we are built before ccache
@@ -51,5 +24,5 @@ HOST_XZ_CONF_ENV = \
 	CC="$(HOSTCC_NOCCACHE)" \
 	CXX="$(HOSTCXX_NOCCACHE)"
 
-$(eval $(cmake-package))
-$(eval $(host-cmake-package))
+$(eval $(autotools-package))
+$(eval $(host-autotools-package))
