f2a9de690c40f591892975742f372cb7c7158f57
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-03-28 00:28:11 +0000

    Update zstd.mk

--- package/zstd/zstd.mk
+++ package/zstd/zstd.mk
@@ -11,92 +11,30 @@ ZSTD_LICENSE = BSD-3-Clause or GPL-2.0
 ZSTD_LICENSE_FILES = LICENSE COPYING
 ZSTD_CPE_ID_VENDOR = facebook
 ZSTD_CPE_ID_PRODUCT = zstandard
+ZSTD_SUBDIR = build/cmake
 
-ZSTD_OPTS += PREFIX=/usr
-ZSTD_OPTS += ZSTD_LEGACY_SUPPORT=0
 ifeq ($(BR2_PACKAGE_ZLIB),y)
 ZSTD_DEPENDENCIES += zlib
-ZSTD_OPTS += HAVE_ZLIB=1
-else
-ZSTD_OPTS += HAVE_ZLIB=0
+ZSTD_CONF_OPTS += -DZSTD_ZLIB_SUPPORT=ON
 endif
 
 ifeq ($(BR2_PACKAGE_XZ),y)
 ZSTD_DEPENDENCIES += xz
-ZSTD_OPTS += HAVE_LZMA=1
-else
-ZSTD_OPTS += HAVE_LZMA=0
+ZSTD_CONF_OPTS += -DZSTD_LZMA_SUPPORT=ON
 endif
 
 ifeq ($(BR2_PACKAGE_LZ4),y)
 ZSTD_DEPENDENCIES += lz4
-ZSTD_OPTS += HAVE_LZ4=1
-else
-ZSTD_OPTS += HAVE_LZ4=0
+ZSTD_CONF_OPTS += -DZSTD_LZ4_SUPPORT=ON
 endif
 
-# zstd will append -O3 after $(CFLAGS), use MOREFLAGS to override again
-ZSTD_OPTS += MOREFLAGS="$(TARGET_OPTIMIZATION)"
-
-ZSTD_BUILD_LIBS_BASENAMES = libzstd.pc
-ifeq ($(BR2_STATIC_LIBS),y)
-ZSTD_BUILD_LIBS_BASENAMES += libzstd.a
-ZSTD_INSTALL_LIBS = install-static
-else ifeq ($(BR2_SHARED_LIBS),y)
-ZSTD_BUILD_LIBS_BASENAMES += lib
-ZSTD_INSTALL_LIBS = install-shared
-else
-ZSTD_BUILD_LIBS_BASENAMES += lib
-ZSTD_INSTALL_LIBS = install-static install-shared
+ifeq ($(BR2_STATIC_LIBS),n)
+ZSTD_CONF_OPTS += -DZSTD_BUILD_STATIC=OFF
 endif
 
-# prefer zstd-dll unless no library is available
-ifeq ($(BR2_STATIC_LIBS),y)
-ZSTD_BUILD_PROG_TARGET = zstd-release
-else
-ZSTD_BUILD_PROG_TARGET = zstd-dll
+ifeq ($(BR2_SHARED_LIBS),n)
+ZSTD_CONF_OPTS += -DZSTD_PROGRAMS_LINK_SHARED=ON
 endif
-
-# The HAVE_THREAD flag is read by the 'programs' makefile but not by  the 'lib'
-# one. Building a multi-threaded binary with a static library (which defaults
-# to single-threaded) gives a runtime error when compressing files.
-# The 'lib' makefile provides specific '%-mt' and '%-nomt' targets for this
-# purpose.
-ifeq ($(BR2_TOOLCHAIN_HAS_THREADS),y)
-ZSTD_OPTS += HAVE_THREAD=1
-ZSTD_BUILD_LIBS_THREAD_SUFFIX = -mt
-else
-ZSTD_OPTS += HAVE_THREAD=0
-ZSTD_BUILD_LIBS_THREAD_SUFFIX = -nomt
-endif
-# check-package disable OverriddenVariable - override intended
-ZSTD_BUILD_LIBS = \
-	$(addsuffix -release, \
-		$(addsuffix $(ZSTD_BUILD_LIBS_THREAD_SUFFIX), \
-			$(ZSTD_BUILD_LIBS_BASENAMES)))
-
-define ZSTD_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) $(ZSTD_OPTS) \
-		-C $(@D)/lib $(ZSTD_BUILD_LIBS)
-	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) $(ZSTD_OPTS) \
-		DESTDIR=$(STAGING_DIR) PREFIX=/usr -C $(@D)/lib \
-		install-pc install-includes $(ZSTD_INSTALL_LIBS)
-	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) $(ZSTD_OPTS) \
-		-C $(@D)/programs $(ZSTD_BUILD_PROG_TARGET)
-endef
-
-define ZSTD_INSTALL_STAGING_CMDS
-	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) $(ZSTD_OPTS) \
-		DESTDIR=$(STAGING_DIR) PREFIX=/usr -C $(@D)/lib \
-		install-pc install-includes $(ZSTD_INSTALL_LIBS)
-endef
-
-define ZSTD_INSTALL_TARGET_CMDS
-	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) $(ZSTD_OPTS) \
-		DESTDIR=$(TARGET_DIR) -C $(@D)/programs install
-	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) $(ZSTD_OPTS) \
-		DESTDIR=$(TARGET_DIR) -C $(@D)/lib $(ZSTD_INSTALL_LIBS)
-endef
 
 HOST_ZSTD_OPTS += PREFIX=$(HOST_DIR)
 HOST_ZSTD_ENV = $(HOST_MAKE_ENV) $(HOST_CONFIGURE_OPTS)
@@ -114,5 +52,5 @@ define HOST_ZSTD_INSTALL_CMDS
 		-C $(@D) install
 endef
 
-$(eval $(generic-package))
+$(eval $(cmake-package))
 $(eval $(host-generic-package))
