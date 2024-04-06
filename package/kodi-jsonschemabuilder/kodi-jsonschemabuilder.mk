c85e7321a7aac2d10866e93a25314ab843eb3751
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-04-06 20:40:49 +0100

    fix kodi build

--- package/kodi-jsonschemabuilder/kodi-jsonschemabuilder.mk
+++ package/kodi-jsonschemabuilder/kodi-jsonschemabuilder.mk
@@ -12,14 +12,14 @@ KODI_JSONSCHEMABUILDER_SOURCE = kodi-$(KODI_JSONSCHEMABUILDER_VERSION).tar.gz
 KODI_JSONSCHEMABUILDER_DL_SUBDIR = kodi
 KODI_JSONSCHEMABUILDER_LICENSE = GPL-2.0
 KODI_JSONSCHEMABUILDER_LICENSE_FILES = LICENSE.md
-HOST_KODI_JSONSCHEMABUILDER_SUBDIR = tools/depends/native/JsonSchemaBuilder
+HOST_KODI_JSONSCHEMABUILDER_SUBDIR = tools/depends/native/JsonSchemaBuilder/src
 
 HOST_KODI_JSONSCHEMABUILDER_CONF_OPTS = \
 	-DCMAKE_MODULE_PATH=$(@D)/project/cmake/modules
 
 define HOST_KODI_JSONSCHEMABUILDER_INSTALL_CMDS
 	$(INSTALL) -m 755 -D \
-		$(@D)/tools/depends/native/JsonSchemaBuilder/JsonSchemaBuilder \
+		$(@D)/tools/depends/native/JsonSchemaBuilder/src/JsonSchemaBuilder \
 		$(HOST_DIR)/bin/kodi-JsonSchemaBuilder
 endef
 
