e3907f62f068d4d37f93bcb0e955fef1069310da
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-05-25 11:14:13 +0100

    rtl network buid speedup

--- package/rtl8821au/rtl8821au.mk
+++ package/rtl8821au/rtl8821au.mk
@@ -21,7 +21,7 @@ endef
 RTL8821AU_MODULE_MAKE_OPTS = \
 	CONFIG_RTL8812AU_8821AU=m \
 # batocera: setting KVER breaks top level parallelization
-	# KVER=$(LINUX_VERSION_PROBED)
+	#KVER=$(LINUX_VERSION_PROBED)
 	USER_EXTRA_CFLAGS="-DCONFIG_$(call qstrip,$(BR2_ENDIAN))_ENDIAN \
 		-Wno-error"
 
