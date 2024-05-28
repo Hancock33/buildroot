cb8b4ff79b09dd8fe0017159658d2a953a12fa19
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-05-28 07:08:52 +0100

    bump gdk-pixbuf version 2.42.12

--- package/gdk-pixbuf-xlib/gdk-pixbuf-xlib.mk
+++ package/gdk-pixbuf-xlib/gdk-pixbuf-xlib.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-GDK_PIXBUF_XLIB_VERSION_MAJOR = 2.40
-GDK_PIXBUF_XLIB_VERSION = $(GDK_PIXBUF_XLIB_VERSION_MAJOR).2
+GDK_PIXBUF_XLIB_VERSION_MAJOR = 2.42
+GDK_PIXBUF_XLIB_VERSION = $(GDK_PIXBUF_XLIB_VERSION_MAJOR).12
 GDK_PIXBUF_XLIB_SOURCE = gdk-pixbuf-xlib-$(GDK_PIXBUF_XLIB_VERSION).tar.xz
 GDK_PIXBUF_XLIB_SITE = https://download.gnome.org/sources/gdk-pixbuf-xlib/$(GDK_PIXBUF_XLIB_VERSION_MAJOR)
 GDK_PIXBUF_XLIB_LICENSE = LGPL-2.1+
