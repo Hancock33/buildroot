8e8acaf6461d6dabbffc1511c29fb8b874fef40a
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-06-01 20:22:47 +0100

    bump 7z version 24.06

--- package/p7zip/p7zip.mk
+++ package/p7zip/p7zip.mk
@@ -4,20 +4,17 @@
 #
 ################################################################################
 
-P7ZIP_VERSION = 2301
+P7ZIP_VERSION = 2406
 P7ZIP_SOURCE = 7z$(P7ZIP_VERSION)-src.tar.xz
 P7ZIP_SITE = https://www.7-zip.org/a
 P7ZIP_LICENSE = LGPL-2.1+ with unRAR restriction
 P7ZIP_LICENSE_FILES = DOC/License.txt
 P7ZIP_CPE_ID_VENDOR = 7-zip
 
-ifeq ($(BR2_PACKAGE_P7ZIP_7ZA),y)
-P7ZIP_TARGET = $(@D)/CPP/7zip/Bundles/Alone
-PZIP_FILE = 7za
-else
-P7ZIP_TARGET = $(@D)/CPP/7zip/Bundles/Alone7z
-PZIP_FILE = 7zr
-endif
+P7A_TARGET = $(@D)/CPP/7zip/Bundles/Alone
+P7A_FILE = 7za
+P7R_TARGET = $(@D)/CPP/7zip/Bundles/Alone7z
+P7R_FILE = 7zr
 
 # p7zip buildsystem is a mess: it plays dirty tricks with CFLAGS and
 # CXXFLAGS, so we can't pass them. Instead, it accepts ALLFLAGS_C
@@ -26,23 +23,27 @@ define P7ZIP_BUILD_CMDS
 	rm -r $(@D)/*
 	cd  $(@D) && tar -xf $(P7ZIP_DL_DIR)/$($(PKG)_SOURCE)
 	$(SED) "s|-O2|$(TARGET_OPTIMIZATION)|g" $(@D)/CPP/7zip/7zip_gcc.mak
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(P7ZIP_TARGET) -f makefile.gcc
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(P7A_TARGET) -f makefile.gcc
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(P7R_TARGET) -f makefile.gcc
 endef
 
 define P7ZIP_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(P7ZIP_TARGET)/_o/$(PZIP_FILE) $(TARGET_DIR)/usr/bin/$(PZIP_FILE)
+	$(INSTALL) -D -m 0755 $(P7A_TARGET)/_o/$(P7A_FILE) $(TARGET_DIR)/usr/bin/$(P7A_FILE)
+	$(INSTALL) -D -m 0755 $(P7R_TARGET)/_o/$(P7R_FILE) $(TARGET_DIR)/usr/bin/$(P7R_FILE)
 endef
 
 # batocera
 define HOST_P7ZIP_BUILD_CMDS
 	rm -r $(@D)/*
 	cd  $(@D) && tar -xf $(P7ZIP_DL_DIR)/$($(PKG)_SOURCE)
-	$(HOST_CONFIGURE_OPTS) $(MAKE) -C $(P7ZIP_TARGET) -f makefile.gcc
+	$(HOST_CONFIGURE_OPTS) $(MAKE) -C $(P7A_TARGET) -f makefile.gcc
+	$(HOST_CONFIGURE_OPTS) $(MAKE) -C $(P7R_TARGET) -f makefile.gcc
 endef
 
 # batocera
 define HOST_P7ZIP_INSTALL_CMDS
-	$(INSTALL) -D -m 0755 $(P7ZIP_TARGET)/_o/$(PZIP_FILE) $(HOST_DIR)/usr/bin/$(PZIP_FILE)
+	$(INSTALL) -D -m 0755 $(P7A_TARGET)/_o/$(P7A_FILE) $(HOST_DIR)/usr/bin/$(P7A_FILE)
+	$(INSTALL) -D -m 0755 $(P7R_TARGET)/_o/$(P7R_FILE) $(HOST_DIR)/usr/bin/$(P7R_FILE)
 endef
 
 $(eval $(generic-package))
