2a895586d0c271442a5de3d0576d78406ca44c2d
Bernd Kuhls <bernd@kuhls.net>
2024-06-06 18:09:25 +0200

    Revert "package/mesa3d: bump version to 24.1.1"
    This reverts commit 50a46a98855854cd67a0daa9ae77b21c6268cb30.

--- package/mesa3d/mesa3d.mk
+++ package/mesa3d/mesa3d.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 # When updating the version, please also update mesa3d-headers
-MESA3D_VERSION = 24.1.1
+MESA3D_VERSION = 24.0.8
 MESA3D_SOURCE = mesa-$(MESA3D_VERSION).tar.xz
 MESA3D_SITE = https://archive.mesa3d.org
 MESA3D_LICENSE = MIT, SGI, Khronos
@@ -124,10 +124,6 @@ MESA3D_CONF_OPTS += \
 	-Dshared-glapi=enabled \
 	-Dgallium-drivers=$(subst $(space),$(comma),$(MESA3D_GALLIUM_DRIVERS-y)) \
 	-Dgallium-extra-hud=true
-endif
-
-ifeq ($(BR2_PACKAGE_MESA3D_GALLIUM_DRIVER_IRIS),y)
-MESA3D_DEPENDENCIES += libclc spirv-llvm-translator spirv-tools
 endif
 
 ifeq ($(BR2_PACKAGE_MESA3D_VULKAN_DRIVER),)
