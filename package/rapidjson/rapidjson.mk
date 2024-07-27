0d8495cf6be4ca323d72bc9c1f04f4e38d038850
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-07-27 21:10:04 +0100

    [1/1] package/rapidjson: bump version

--- package/rapidjson/rapidjson.mk
+++ package/rapidjson/rapidjson.mk
@@ -3,9 +3,9 @@
 # rapidjson
 #
 ################################################################################
-
+# Version: Commits on Jan 20, 2024
 RAPIDJSON_VERSION = ab1842a2dae061284c0a62dca1cc6d5e7e37e346
-RAPIDJSON_SITE = $(call github,miloyip,rapidjson,$(RAPIDJSON_VERSION))
+RAPIDJSON_SITE = $(call github,Tencent,rapidjson,$(RAPIDJSON_VERSION))
 RAPIDJSON_LICENSE = MIT
 RAPIDJSON_LICENSE_FILES = license.txt
 RAPIDJSON_CPE_ID_VENDOR = tencent
