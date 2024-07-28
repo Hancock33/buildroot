dfcb8d95a923f23439a4ef809b6905cff3f578e1
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-07-28 15:35:38 +0100

    tryout llvm 19.1.0-rc1

--- package/llvm-project/llvm-project.mk
+++ package/llvm-project/llvm-project.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-LLVM_PROJECT_VERSION_MAJOR = 18
-LLVM_PROJECT_VERSION = $(LLVM_PROJECT_VERSION_MAJOR).1.8
-LLVM_PROJECT_SITE = https://github.com/llvm/llvm-project/releases/download/llvmorg-$(subst rc,-rc,$(LLVM_PROJECT_VERSION))
+LLVM_PROJECT_VERSION_MAJOR = 19
+LLVM_PROJECT_VERSION = $(LLVM_PROJECT_VERSION_MAJOR).1.0-rc1
+LLVM_PROJECT_SITE = https://github.com/llvm/llvm-project/releases/download/llvmorg-$(subst rc,rc,$(LLVM_PROJECT_VERSION))
 
 include $(sort $(wildcard package/llvm-project/*/*.mk))
