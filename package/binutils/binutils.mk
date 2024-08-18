ee1d0878e8c6dfb899e34eeccc2224018c06e5fc
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-08-18 20:16:50 +0100

    Update binutils.mk

--- package/binutils/binutils.mk
+++ package/binutils/binutils.mk
@@ -23,7 +23,7 @@ endif
 
 ifeq ($(BR2_BINUTILS_VERSION_GIT),y)
 #git describe --abbrev=40 origin/binutils-2_43-branch | cut -d '-' -f 2-
-	BINUTILS_VERSION = ddf04874fa6ec0a0d10f813b245c33ce19c50473
+	BINUTILS_VERSION = 2_43_1-2-g1e0ae22b6d25c020356ea86e0fdf1a9a0c7c74c2
 	BINUTILS_SITE = $(call github,sailfishos-mirror,gdb,$(BINUTILS_VERSION))
 	BINUTILS_SOURCE = binutils-$(BINUTILS_VERSION).tar.gz
 	BINUTILS_FROM_GIT = y
