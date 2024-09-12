278df41dc870ca4c46c1cd24bd90f23f13016678
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-09-12 23:30:18 +0100

    bump python-glslang version 14.3.0

--- package/python-glslang/python-glslang.mk
+++ package/python-glslang/python-glslang.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_GLSLANG_VERSION = 11.13.0
+PYTHON_GLSLANG_VERSION = 14.3.0
 PYTHON_GLSLANG_SITE = $(call github,KhronosGroup,glslang,$(PYTHON_GLSLANG_VERSION))
 PYTHON_GLSLANG_LICENSE = BSD-3-Clause
 PYTHON_GLSLANG_LICENSE_FILES = LICENSE.txt
