5918c75ef1f7a154613927cce2890aa7560ce214
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-09-14 11:05:19 +0100

    bump libplatorm to the lastest commit

--- package/libplatform/libplatform.mk
+++ package/libplatform/libplatform.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBPLATFORM_VERSION = a7cd0d5780ed80a4e70480d1650749f29e8a1fb2
+LIBPLATFORM_VERSION = 5a786dd580972a2dc89941ea60dc08c0314e0e4d
 LIBPLATFORM_SITE = $(call github,Pulse-Eight,platform,$(LIBPLATFORM_VERSION))
 LIBPLATFORM_LICENSE = GPL-2.0+
 LIBPLATFORM_LICENSE_FILES = src/os.h
