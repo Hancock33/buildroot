e238d640cd60c77131e2ce73f7e096db0b77a3b4
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-09-14 01:03:15 +0100

    correct host-python-setuptools-rust filename

--- package/python-setuptools-rust/python-setuptools-rust.mk
+++ package/python-setuptools-rust/python-setuptools-rust.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 PYTHON_SETUPTOOLS_RUST_VERSION = 1.10.1
-PYTHON_SETUPTOOLS_RUST_SOURCE = setuptools-rust-$(PYTHON_SETUPTOOLS_RUST_VERSION).tar.gz
+PYTHON_SETUPTOOLS_RUST_SOURCE = setuptools_rust-$(PYTHON_SETUPTOOLS_RUST_VERSION).tar.gz
 PYTHON_SETUPTOOLS_RUST_SITE = https://files.pythonhosted.org/packages/b8/86/4f34594f21f529623b8650fe729548e3a2ad6c9ad81583391f03f74dd11a
 PYTHON_SETUPTOOLS_RUST_SETUP_TYPE = setuptools
 PYTHON_SETUPTOOLS_RUST_LICENSE = MIT
