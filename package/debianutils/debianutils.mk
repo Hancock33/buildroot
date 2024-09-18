40de32df5b55732d12daf5a14ce317fd025f8865
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-09-18 20:25:47 +0100

    bump debianutils version 5.20

--- package/debianutils/debianutils.mk
+++ package/debianutils/debianutils.mk
@@ -4,9 +4,10 @@
 #
 ################################################################################
 
-DEBIANUTILS_VERSION = 4.11
+DEBIANUTILS_VERSION = 5.20
 DEBIANUTILS_SOURCE = debianutils_$(DEBIANUTILS_VERSION).tar.xz
-DEBIANUTILS_SITE = http://snapshot.debian.org/archive/debian/20200525T145753Z/pool/main/d/debianutils
+DEBIANUTILS_SITE = https://snapshot.debian.org/archive/debian/20240918T144258Z/pool/main/d/debianutils
+DEBIANUTILS_AUTORECONF = YES
 DEBIANUTILS_CONF_OPTS = --exec-prefix=/
 DEBIANUTILS_LICENSE = GPL-2.0+, SMAIL (savelog)
 DEBIANUTILS_LICENSE_FILES = debian/copyright
