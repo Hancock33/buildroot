0f7627d7c0fea101f60651b20019da3b014fad58
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-09-26 13:07:35 +0100

    update python-glslang version 15.0.0

--- package/python-glslang/python-glslang.mk
+++ package/python-glslang/python-glslang.mk
@@ -3,8 +3,8 @@
 # python-glslang
 #
 ################################################################################
-
-PYTHON_GLSLANG_VERSION = 14.3.0
+# Version: Commits on Sept 25, 2024
+PYTHON_GLSLANG_VERSION = 15.0.0
 PYTHON_GLSLANG_SITE = $(call github,KhronosGroup,glslang,$(PYTHON_GLSLANG_VERSION))
 PYTHON_GLSLANG_LICENSE = BSD-3-Clause
 PYTHON_GLSLANG_LICENSE_FILES = LICENSE.txt
