5fe817a929c0a764578a007c7c73aa3e88e77e32
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-10-10 05:50:09 +0100

    update qt6wayland

--- package/qt6/qt6wayland/qt6wayland.mk
+++ package/qt6/qt6wayland/qt6wayland.mk
@@ -11,28 +11,34 @@ QT6WAYLAND_INSTALL_STAGING = YES
 QT6WAYLAND_SUPPORTS_IN_SOURCE_BUILD = NO
 QT6WAYLAND_CMAKE_BACKEND = ninja
 
-QT6WAYLAND_DEPENDENCIES = qt6base host-qt6wayland qt6declarative
-
-HOST_QT6WAYLAND_CONF_OPTS = \
-    -DQT_HOST_PATH=$(HOST_DIR) \
-    -DFEATURE_wayland_server=OFF \
-    $(QT6_COMMON_CONF_OPTS)
+# host-qt6wayland needed to build the qtwaylandscanner host tool
+QT6WAYLAND_DEPENDENCIES = qt6base wayland host-qt6wayland
 
 QT6WAYLAND_LICENSE = \
-    GPL-2.0+ or LGPL-3.0, \
-    GPL-3.0, GFDL-1.3 no invariants (docs)
+	BSD-3-Clause (examples, tests), \
+	GPL-3.0 (compositor), \
+	LGPL-3.0 or GPL-2.0 or GPL-3.0, \
+	GPL-3.0 WITH Qt-GPL-exception-1.0 (tests), \
+	GFDL-1.3 no invariants (docs)
 
 QT6WAYLAND_LICENSE_FILES = \
-    LICENSES/GPL-2.0-only.txt \
-    LICENSES/GPL-3.0-only.txt \
-    LICENSES/LGPL-3.0-only.txt \
-    LICENSES/GFDL-1.3-no-invariants-only.txt
+	LICENSES/BSD-3-Clause.txt \
+	LICENSES/GPL-2.0-only.txt \
+	LICENSES/GPL-3.0-only.txt \
+	LICENSES/LGPL-3.0-only.txt \
+	LICENSES/GFDL-1.3-no-invariants-only.txt
 
 QT6WAYLAND_CONF_OPTS = \
 	-DQT_HOST_PATH=$(HOST_DIR) \
+	-DFEATURE_wayland_client=ON \
 	$(QT6_COMMON_CONF_OPTS)
 
-QT6WAYLAND_DEPENDENCIES = qt6base host-qt6wayland
+
+HOST_QT6WAYLAND_DEPENDENCIES = host-qt6base
+HOST_QT6WAYLAND_CONF_OPTS = \
+	-DFEATURE_wayland_client=OFF \
+	-DFEATURE_wayland_server=OFF \
+	$(QT6_COMMON_CONF_OPTS)
 
 $(eval $(cmake-package))
 $(eval $(host-cmake-package))
