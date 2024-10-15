c0d1f401d9e064e8c49c1336ad6dfd0cf0874a77
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-10-15 20:14:00 +0100

    bump llvm version 19.1.2

--- package/llvm-project/llvm-project.mk
+++ package/llvm-project/llvm-project.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LLVM_PROJECT_VERSION_MAJOR = 19
-LLVM_PROJECT_VERSION = $(LLVM_PROJECT_VERSION_MAJOR).1.1
+LLVM_PROJECT_VERSION = $(LLVM_PROJECT_VERSION_MAJOR).1.2
 #LLVM_PROJECT_SITE = https://github.com/llvm/llvm-project/releases/download/llvmorg-$(subst rc,-rc,$(LLVM_PROJECT_VERSION))
 LLVM_PROJECT_SITE = https://github.com/llvm/llvm-project/releases/download/llvmorg-$(LLVM_PROJECT_VERSION)
 
