5518d8dd2ee61a8c3b28d57cc6b64188801ac846
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-10-16 07:06:55 +0100

    drop xlib_libxfont

--- package/x11r7/xapp_bdftopcf/xapp_bdftopcf.mk
+++ package/x11r7/xapp_bdftopcf/xapp_bdftopcf.mk
@@ -9,8 +9,8 @@ XAPP_BDFTOPCF_SOURCE = bdftopcf-$(XAPP_BDFTOPCF_VERSION).tar.xz
 XAPP_BDFTOPCF_SITE = https://xorg.freedesktop.org/archive/individual/util
 XAPP_BDFTOPCF_LICENSE = MIT
 XAPP_BDFTOPCF_LICENSE_FILES = COPYING
-XAPP_BDFTOPCF_DEPENDENCIES = xlib_libXfont
-HOST_XAPP_BDFTOPCF_DEPENDENCIES = host-xlib_libXfont
+XAPP_BDFTOPCF_DEPENDENCIES = xlib_libXfont2
+HOST_XAPP_BDFTOPCF_DEPENDENCIES = host-xlib_libXfont2
 
 # needed for linking against libXfont
 XAPP_BDFTOPCF_MAKE_OPTS += LIBS=-ldl
