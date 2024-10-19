7e1fbb6c8d3c1571a38cf968459ac2993a707eda
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-10-19 08:08:55 +0100

    bump vim version 9.1.0792

--- package/vim/vim.mk
+++ package/vim/vim.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-VIM_VERSION = 9.1.0789
+VIM_VERSION = 9.1.0792
 VIM_SITE = $(call github,vim,vim,v$(VIM_VERSION))
 VIM_DEPENDENCIES = ncurses $(TARGET_NLS_DEPENDENCIES)
 VIM_SUBDIR = src
