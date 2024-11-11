dc45515137e964cd6cd3a552ccb06a425f6c6b12
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-11-11 21:05:04 +0000

    update python-tomli version 2.1.0

--- package/python-tomli/python-tomli.mk
+++ package/python-tomli/python-tomli.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-PYTHON_TOMLI_VERSION = 2.0.2
+PYTHON_TOMLI_VERSION = 2.1.0
 PYTHON_TOMLI_SOURCE = tomli-$(PYTHON_TOMLI_VERSION).tar.gz
-PYTHON_TOMLI_SITE = https://files.pythonhosted.org/packages/35/b9/de2a5c0144d7d75a57ff355c0c24054f965b2dc3036456ae03a51ea6264b
+PYTHON_TOMLI_SITE = https://files.pythonhosted.org/packages/1e/e4/1b6cbcc82d8832dd0ce34767d5c560df8a3547ad8cbc427f34601415930a
+PYTHON_TOMLI_SETUP_TYPE = flit
 PYTHON_TOMLI_LICENSE = MIT
 PYTHON_TOMLI_LICENSE_FILES = LICENSE
-PYTHON_TOMLI_SETUP_TYPE = flit
 
 $(eval $(python-package))
 $(eval $(host-python-package))
