9510df07029dc927ae3c83e0446cd4f0800d3e71
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-11-22 15:11:31 +0000

    update python-yarl version 1.18.0

--- package/python-yarl/python-yarl.mk
+++ package/python-yarl/python-yarl.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_YARL_VERSION = 1.17.2
+PYTHON_YARL_VERSION = 1.18.0
 PYTHON_YARL_SOURCE = yarl-$(PYTHON_YARL_VERSION).tar.gz
-PYTHON_YARL_SITE = https://files.pythonhosted.org/packages/4b/d5/0d0481857de42a44ba4911f8010d4b361dc26487f48d5503c66a797cff48
+PYTHON_YARL_SITE = https://files.pythonhosted.org/packages/5e/4b/53db4ecad4d54535aff3dfda1f00d6363d79455f62b11b8ca97b82746bd2
 PYTHON_YARL_LICENSE = Apache-2.0
 PYTHON_YARL_LICENSE_FILES = LICENSE
 PYTHON_YARL_SETUP_TYPE = setuptools
