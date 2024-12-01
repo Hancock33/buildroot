400749e607df74061abbff76c630cb521d0ffa5f
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-12-01 10:15:16 +0000

    spirv-llvm-translator version 19.1.2

--- package/spirv-llvm-translator/spirv-llvm-translator.mk
+++ package/spirv-llvm-translator/spirv-llvm-translator.mk
@@ -3,8 +3,8 @@
 # spirv-llvm-translator
 #
 ################################################################################
-# Version: Commits on Oct 30, 2024
-SPIRV_LLVM_TRANSLATOR_VERSION = v19.1.1
+# Version: Commits on Nov 28, 2024
+SPIRV_LLVM_TRANSLATOR_VERSION = v19.1.2
 SPIRV_LLVM_TRANSLATOR_SITE = $(call github,KhronosGroup,SPIRV-LLVM-Translator,$(SPIRV_LLVM_TRANSLATOR_VERSION))
 SPIRV_LLVM_TRANSLATOR_LICENSE = NCSA
 SPIRV_LLVM_TRANSLATOR_LICENSE_FILES = LICENSE.TXT
