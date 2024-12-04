4f7ca8cccb78f16f80ea5417b1d2eb4edc43b000
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2024-12-04 18:07:30 +0000

    update unrar version 7.1.2

--- package/unrar/unrar.mk
+++ package/unrar/unrar.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UNRAR_VERSION = 7.1.1
+UNRAR_VERSION = 7.1.2
 UNRAR_SOURCE = unrarsrc-$(UNRAR_VERSION).tar.gz
 UNRAR_SITE = https://www.rarlab.com/rar
 UNRAR_LICENSE = unrar
