bd126ab54e817652fbc66b2d461d683f34b7903f
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-01-01 09:39:19 +0000

    Update gettext-tiny.mk

--- package/gettext-tiny/gettext-tiny.mk
+++ package/gettext-tiny/gettext-tiny.mk
@@ -23,11 +23,12 @@ GETTEXT_TINY_EXTRA_GETTEXT_FILES = \
 	gettext-runtime/po/boldquot.sed \
 	gettext-runtime/po/en@boldquot.header \
 	gettext-runtime/po/en@quot.header \
-	gettext-runtime/po/insert-header.sin \
-	gettext-runtime/po/quot.sed \
-	gettext-runtime/po/remove-potcdate.sin \
-	gettext-runtime/po/Rules-quot \
-	gettext-runtime/po/Makefile.in.in \
+	gettext-runtime/po/gettextize.in \
+	#gettext-runtime/po/insert-header.sin \
+	#gettext-runtime/po/quot.sed \
+	#gettext-runtime/po/remove-potcdate.sin \
+	#gettext-runtime/po/Rules-quot \
+	#gettext-runtime/po/Makefile.in.in \
 	COPYING
 
 HOST_GETTEXT_TINY_EXTRA_DOWNLOADS = $(GETTEXT_GNU_SITE)/$(GETTEXT_GNU_SOURCE)
@@ -83,14 +84,15 @@ define HOST_GETTEXT_TINY_INSTALL_CMDS
 
 	$(INSTALL) -m 0755 -D $(@D)/gettextize $(HOST_DIR)/bin/gettextize
 	$(INSTALL) -m 0644 -D $(@D)/build-aux/config.rpath $(HOST_DIR)/share/gettext-tiny/config.rpath
-	$(INSTALL) -m 0644 -D $(@D)/extra/Makefile.in.in $(HOST_DIR)/share/gettext-tiny/po/Makefile.in.in
+	#$(INSTALL) -m 0644 -D $(@D)/extra/Makefile.in.in $(HOST_DIR)/share/gettext-tiny/po/Makefile.in.in
 	$(INSTALL) -m 0644 -D $(@D)/extra/boldquot.sed $(HOST_DIR)/share/gettext-tiny/po/boldquot.sed
 	$(INSTALL) -m 0644 -D $(@D)/extra/en@boldquot.header $(HOST_DIR)/share/gettext-tiny/po/en@boldquot.header
 	$(INSTALL) -m 0644 -D $(@D)/extra/en@quot.header $(HOST_DIR)/share/gettext-tiny/po/en@quot.header
-	$(INSTALL) -m 0644 -D $(@D)/extra/insert-header.sin $(HOST_DIR)/share/gettext-tiny/po/insert-header.sin
-	$(INSTALL) -m 0644 -D $(@D)/extra/quot.sed $(HOST_DIR)/share/gettext-tiny/po/quot.sed
-	$(INSTALL) -m 0644 -D $(@D)/extra/remove-potcdate.sin $(HOST_DIR)/share/gettext-tiny/po/remove-potcdate.sin
-	$(INSTALL) -m 0644 -D $(@D)/extra/Rules-quot $(HOST_DIR)/share/gettext-tiny/po/Rules-quot
+	$(INSTALL) -m 0644 -D $(@D)/extra/gettextize.in $(HOST_DIR)/share/gettext-tiny/po/gettextize.in
+	#$(INSTALL) -m 0644 -D $(@D)/extra/insert-header.sin $(HOST_DIR)/share/gettext-tiny/po/insert-header.sin
+	#$(INSTALL) -m 0644 -D $(@D)/extra/quot.sed $(HOST_DIR)/share/gettext-tiny/po/quot.sed
+	#$(INSTALL) -m 0644 -D $(@D)/extra/remove-potcdate.sin $(HOST_DIR)/share/gettext-tiny/po/remove-potcdate.sin
+	#$(INSTALL) -m 0644 -D $(@D)/extra/Rules-quot $(HOST_DIR)/share/gettext-tiny/po/Rules-quot
 	$(INSTALL) -m 0644 -D $(@D)/extra/Makevars.template $(HOST_DIR)/share/gettext-tiny/po/Makevars.template
 
 	$(Q)touch $(HOST_DIR)/share/gettext-tiny/ABOUT-NLS
