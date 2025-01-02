23c4cd54bf56e31b6c7cfd2f4a57248d53053adf
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-01-02 19:20:57 +0000

    update python-ruamel-yaml version 0.18.8

--- package/python-ruamel-yaml/python-ruamel-yaml.mk
+++ package/python-ruamel-yaml/python-ruamel-yaml.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_RUAMEL_YAML_VERSION = 0.18.7
+PYTHON_RUAMEL_YAML_VERSION = 0.18.8
 PYTHON_RUAMEL_YAML_SOURCE = ruamel.yaml-$(PYTHON_RUAMEL_YAML_VERSION).tar.gz
-PYTHON_RUAMEL_YAML_SITE = https://files.pythonhosted.org/packages/be/f6/ae958c14677098f4fcc0a6b04b382c02f31ec628d2b03910e24f29d9bc58
+PYTHON_RUAMEL_YAML_SITE = https://files.pythonhosted.org/packages/4a/9f/a073b2c0d916045c3decf885b6fea12d2d1b02f235e89a02606140111138
 PYTHON_RUAMEL_YAML_SETUP_TYPE = setuptools
 PYTHON_RUAMEL_YAML_LICENSE = MIT
 PYTHON_RUAMEL_YAML_LICENSE_FILES = LICENSE
