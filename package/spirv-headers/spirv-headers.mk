06000ca3b0ddd907903112c00fa66aae39dbb876
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-01-14 18:22:38 +0000

    bump SPIRV-Headers version 1.4.304.0

--- package/spirv-headers/spirv-headers.mk
+++ package/spirv-headers/spirv-headers.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version: Commits on Dec 04, 2024
-SPIRV_HEADERS_VERSION = 3f17b2af6784bfa2c5aa5dbb8e0e74a607dd8b3b
+SPIRV_HEADERS_VERSION = vulkan-sdk-1.4.304.0
 SPIRV_HEADERS_SITE = $(call github,KhronosGroup,SPIRV-Headers,$(SPIRV_HEADERS_VERSION))
 SPIRV_HEADERS_LICENSE = MIT
 SPIRV_HEADERS_LICENSE_FILES = LICENSE
