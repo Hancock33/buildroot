98c5b8d4737671a9df00d7faf5397a0a7c696770
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-01-16 18:27:08 +0000

    Update sdl2.mk

--- package/sdl2/sdl2.mk
+++ package/sdl2/sdl2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # git describe --tags --abbrev=40 origin/release-2.28.x | cut -d '-' -f 2-
-SDL2_VERSION = release-2.30.11
+SDL2_VERSION = 2.30.11-11-g63521cb9a786e8a3a30242f163af3bccff6febd2
 SDL2_SITE = $(call github,libsdl-org,SDL,$(SDL2_VERSION))
 SDL2_LICENSE = Zlib
 SDL2_LICENSE_FILES = LICENSE.txt
