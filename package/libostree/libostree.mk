5e1eb8c749cad0ab155fc8678345ad0d248cae7d
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-01-17 18:10:50 +0000

    update libostree version 2025.1

--- package/libostree/libostree.mk
+++ package/libostree/libostree.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBOSTREE_VERSION = 2024.9
+LIBOSTREE_VERSION = 2025.1
 LIBOSTREE_SOURCE = libostree-$(LIBOSTREE_VERSION).tar.xz
 LIBOSTREE_SITE = https://github.com/ostreedev/ostree/releases/download/v$(LIBOSTREE_VERSION)
 
