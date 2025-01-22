ac76d2672ea584f5ef87ec3b81f4ebf1420894d9
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-01-22 18:59:14 +0000

    update flatbuffers version 25.1.21

--- package/flatbuffers/flatbuffers.mk
+++ package/flatbuffers/flatbuffers.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FLATBUFFERS_VERSION = 24.12.23
+FLATBUFFERS_VERSION = 25.1.21
 FLATBUFFERS_SITE = $(call github,google,flatbuffers,v$(FLATBUFFERS_VERSION))
 FLATBUFFERS_LICENSE = Apache-2.0
 FLATBUFFERS_LICENSE_FILES = LICENSE
