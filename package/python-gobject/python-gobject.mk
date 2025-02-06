d5fdbd399794b1eef52b290f157c2695724ad961
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-02-06 21:01:58 +0000

    update python-gobject version 3.51.0

--- package/python-gobject/python-gobject.mk
+++ package/python-gobject/python-gobject.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_GOBJECT_VERSION_MAJOR = 3.50
+PYTHON_GOBJECT_VERSION_MAJOR = 3.51
 PYTHON_GOBJECT_VERSION = $(PYTHON_GOBJECT_VERSION_MAJOR).0
 PYTHON_GOBJECT_SOURCE = pygobject-$(PYTHON_GOBJECT_VERSION).tar.xz
 PYTHON_GOBJECT_SITE = https://download.gnome.org/sources/pygobject/$(PYTHON_GOBJECT_VERSION_MAJOR)
