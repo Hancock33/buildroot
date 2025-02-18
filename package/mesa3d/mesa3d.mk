51d5698615c73a0f273e5b5be54ce90bacfd1ade
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-02-18 21:39:24 +0000

    Update mesa3d.mk

--- package/mesa3d/mesa3d.mk
+++ package/mesa3d/mesa3d.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # git describe --abbrev=40 origin/staging/25.0 | cut -d '-' -f 2-
-MESA3D_VERSION = mesa-25.0.0-rc3
+MESA3D_VERSION = 25.0.0-rc3-23-gdc633a35603deada1b01553a00dbc5bc67326e26
 MESA3D_SITE = $(call gitlabfreedesktop,mesa,mesa,$(MESA3D_VERSION))
 MESA3D_LICENSE = MIT, SGI, Khronos
 MESA3D_LICENSE_FILES = docs/license.rst
