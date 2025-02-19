9e2d5cb4067eb1926a90909f9df36d0b9819cee4
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-02-19 06:42:59 +0000

    update util-linux-libs version 2.42-start

--- package/unrar/unrar.mk
+++ package/unrar/unrar.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UNRAR_VERSION = 7.1.3
+UNRAR_VERSION = 7.1.4
 UNRAR_SOURCE = unrarsrc-$(UNRAR_VERSION).tar.gz
 UNRAR_SITE = https://www.rarlab.com/rar
 UNRAR_LICENSE = unrar
