22dc33826572e7388cb2cf57421019de19c8e8c4
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-03-07 18:48:39 +0000

    Revert "update vulkan"
    This reverts commit 8276e1c3a62b2ebd4836b63e0de38c777cd4c81a.

--- package/spirv-tools/spirv-tools.mk
+++ package/spirv-tools/spirv-tools.mk
@@ -3,8 +3,8 @@
 # spirv-tools
 #
 ################################################################################
-# Version: Commits on Feb 25, 2025
-SPIRV_TOOLS_VERSION = v2025.1.rc1
+# Version: Commits on Dec 05, 2024
+SPIRV_TOOLS_VERSION = $(SPIRV_HEADERS_VERSION)
 SPIRV_TOOLS_SITE = $(call github,KhronosGroup,SPIRV-Tools,$(SPIRV_TOOLS_VERSION))
 SPIRV_TOOLS_LICENSE = Apache-2.0
 SPIRV_TOOLS_LICENSE_FILES = LICENSE
