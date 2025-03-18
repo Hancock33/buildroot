e35a075534f7267668ed6607a4dbefca892e9c3c
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-03-18 21:50:48 +0000

    update python-setuptools version 76.1.0

--- package/python-setuptools/python-setuptools.mk
+++ package/python-setuptools/python-setuptools.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SETUPTOOLS_VERSION = 76.0.0
+PYTHON_SETUPTOOLS_VERSION = 76.1.0
 PYTHON_SETUPTOOLS_SOURCE = setuptools-$(PYTHON_SETUPTOOLS_VERSION).tar.gz
-PYTHON_SETUPTOOLS_SITE = https://files.pythonhosted.org/packages/32/d2/7b171caf085ba0d40d8391f54e1c75a1cda9255f542becf84575cfd8a732
+PYTHON_SETUPTOOLS_SITE = https://files.pythonhosted.org/packages/fa/2b/287ade3a580869e6178cb37d045f54272b1f006f2c0ff6fad08db258d027
 PYTHON_SETUPTOOLS_LICENSE = MIT
 PYTHON_SETUPTOOLS_LICENSE_FILES = LICENSE
 PYTHON_SETUPTOOLS_CPE_ID_VENDOR = python
