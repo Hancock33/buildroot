e04f8e9164cc93bf242382979f5d5effdb717af6
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-03-25 18:40:26 +0000

    update python-pyparsing version 3.2.3

--- package/python-pyparsing/python-pyparsing.mk
+++ package/python-pyparsing/python-pyparsing.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYPARSING_VERSION = 3.2.1
+PYTHON_PYPARSING_VERSION = 3.2.3
 PYTHON_PYPARSING_SOURCE = pyparsing-$(PYTHON_PYPARSING_VERSION).tar.gz
-PYTHON_PYPARSING_SITE = https://files.pythonhosted.org/packages/8b/1a/3544f4f299a47911c2ab3710f534e52fea62a633c96806995da5d25be4b2
+PYTHON_PYPARSING_SITE = https://files.pythonhosted.org/packages/bb/22/f1129e69d94ffff626bdb5c835506b3a5b4f3d070f17ea295e12c2c6f60f
 PYTHON_PYPARSING_LICENSE = MIT
 PYTHON_PYPARSING_LICENSE_FILES = LICENSE
 PYTHON_PYPARSING_SETUP_TYPE = flit
