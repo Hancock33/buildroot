7990079cb03a6269f716707e0bba223d3ddebf29
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-04-01 07:10:35 +0100

    update python-calver version 2025.3.31

--- package/python-calver/python-calver.mk
+++ package/python-calver/python-calver.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_CALVER_VERSION = 2022.6.26
+PYTHON_CALVER_VERSION = 2025.3.31
 PYTHON_CALVER_SOURCE = calver-$(PYTHON_CALVER_VERSION).tar.gz
-PYTHON_CALVER_SITE = https://files.pythonhosted.org/packages/b5/00/96cbed7c019c49ee04b8a08357a981983db7698ae6de402e57097cefc9ad
+PYTHON_CALVER_SITE = PYTHON_CALVER_SITE = https://files.pythonhosted.org/packages/58/d7/ebd18f710cedddc010cc71d89ceb00425fc373ec44311aa66ed57790432d
 PYTHON_CALVER_SETUP_TYPE = setuptools
 PYTHON_CALVER_LICENSE = Apache-2.0
 PYTHON_CALVER_LICENSE_FILES = LICENSE
