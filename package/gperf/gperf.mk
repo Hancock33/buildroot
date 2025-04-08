18f7f8e2609a04ec3f7374b8b4a9601d8d0c59b3
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-04-08 07:21:23 +0100

    update gperf version 3.2

--- package/gperf/gperf.mk
+++ package/gperf/gperf.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GPERF_VERSION = 3.1
+GPERF_VERSION = 3.2
 GPERF_SITE = $(BR2_GNU_MIRROR)/gperf
 GPERF_LICENSE = GPL-3.0+
 GPERF_LICENSE_FILES = COPYING
