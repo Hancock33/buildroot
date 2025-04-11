26787b17d8b5fb52df1ea78a3af1467697ae64ee
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-04-11 07:19:43 +0100

    update python-multidict version 6.4.3

--- package/python-multidict/python-multidict.mk
+++ package/python-multidict/python-multidict.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_MULTIDICT_VERSION = 6.4.2
+PYTHON_MULTIDICT_VERSION = 6.4.3
 PYTHON_MULTIDICT_SOURCE = multidict-$(PYTHON_MULTIDICT_VERSION).tar.gz
-PYTHON_MULTIDICT_SITE = https://files.pythonhosted.org/packages/79/f2/5e10ba356eddf43b85b67df6fdd07dce882cc8479cfe434e97cc72603ac9
+PYTHON_MULTIDICT_SITE = https://files.pythonhosted.org/packages/da/2c/e367dfb4c6538614a0c9453e510d75d66099edf1c4e69da1b5ce691a1931
 PYTHON_MULTIDICT_SETUP_TYPE = setuptools
 PYTHON_MULTIDICT_LICENSE = Apache-2.0
 PYTHON_MULTIDICT_LICENSE_FILES = LICENSE
