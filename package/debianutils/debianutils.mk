b11e4e5abe4c56ed0c26d6ad81ac33542c54f25b
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-04-12 09:35:00 +0100

    update debianutils version 5.22

--- package/debianutils/debianutils.mk
+++ package/debianutils/debianutils.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-DEBIANUTILS_VERSION = 5.21
+DEBIANUTILS_VERSION = 5.22
 DEBIANUTILS_SOURCE = debianutils_$(DEBIANUTILS_VERSION).tar.xz
-DEBIANUTILS_SITE = https://snapshot.debian.org/archive/debian/20241120T204342Z/pool/main/d/debianutils
+DEBIANUTILS_SITE = https://snapshot.debian.org/archive/debian/20250412T025124Z/pool/main/d/debianutils
 DEBIANUTILS_AUTORECONF = YES
 DEBIANUTILS_CONF_OPTS = --exec-prefix=/
 DEBIANUTILS_LICENSE = GPL-2.0+, SMAIL (savelog)
