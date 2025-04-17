71ba27402ac55ee2094dabfd73f5210d634a1832
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-04-17 21:11:52 +0100

    update libxml2 version 2.14.2

--- package/python-calver/python-calver.mk
+++ package/python-calver/python-calver.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_CALVER_VERSION = 2025.4.2
+PYTHON_CALVER_VERSION = 2025.4.17
 PYTHON_CALVER_SOURCE = calver-$(PYTHON_CALVER_VERSION).tar.gz
-PYTHON_CALVER_SITE = https://files.pythonhosted.org/packages/ee/28/c6c465ab2ce1882eb1ba8e0f173c1613f509eba3c60eb916fcb9b704238c
+PYTHON_CALVER_SITE = https://files.pythonhosted.org/packages/4f/1e/6cdde2a9cbd1bd2fad6fab0d005b1c10b4161823656ddb19ccc3adcbd5af
 PYTHON_CALVER_SETUP_TYPE = setuptools
 PYTHON_CALVER_LICENSE = Apache-2.0
 PYTHON_CALVER_LICENSE_FILES = LICENSE
