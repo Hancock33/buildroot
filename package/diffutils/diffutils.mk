7874c5519fbd22dcf2ac75ce9ce6eb0342b2de5e
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-04-18 08:00:17 +0100

    update diffutils version 3.12

--- package/diffutils/diffutils.mk
+++ package/diffutils/diffutils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DIFFUTILS_VERSION = 3.11
+DIFFUTILS_VERSION = 3.12
 DIFFUTILS_SOURCE = diffutils-$(DIFFUTILS_VERSION).tar.xz
 DIFFUTILS_SITE = $(BR2_GNU_MIRROR)/diffutils
 DIFFUTILS_DEPENDENCIES = $(TARGET_NLS_DEPENDENCIES)
@@ -15,7 +15,7 @@ DIFFUTILS_CPE_ID_VENDOR = gnu
 # Since glibc >= 2.26, don't try to use getopt_long replacement bundled
 # with diffutils. It will conflict with the one from glibc.
 ifeq ($(BR2_TOOLCHAIN_USES_GLIBC),y)
-DIFFUTILS_CONF_ENV += gl_cv_func_getopt_gnu=yes
+DIFFUTILS_CONF_ENV += gl_cv_func_getopt_gnu=yes  gl_cv_func_strcasecmp_works=yes
 endif
 
 $(eval $(autotools-package))
