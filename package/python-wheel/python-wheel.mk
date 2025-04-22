82628d5a9994e1b01784e2abbdeb103576c31a00
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-04-22 20:55:46 +0100

    update python-wheel version 0.46.1

--- package/python-wheel/python-wheel.mk
+++ package/python-wheel/python-wheel.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_WHEEL_VERSION = 0.45.1
+PYTHON_WHEEL_VERSION = 0.46.1
 PYTHON_WHEEL_SOURCE = wheel-$(PYTHON_WHEEL_VERSION).tar.gz
-PYTHON_WHEEL_SITE = https://files.pythonhosted.org/packages/8a/98/2d9906746cdc6a6ef809ae6338005b3f21bb568bea3165cfc6a243fdc25c
+PYTHON_WHEEL_SITE = https://files.pythonhosted.org/packages/fb/62/e90918c4558b002726ab930863c0cbd3e7cf9a7befa1d4a1a240cecdb379
 PYTHON_WHEEL_SETUP_TYPE = flit
 PYTHON_WHEEL_LICENSE = MIT
 PYTHON_WHEEL_LICENSE_FILES = LICENSE.txt
