c68651cb9964a053a2e7a3116e2484b9c20b1528
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-04-27 19:29:14 +0100

    Update mold.mk

--- package/mold/mold.mk
+++ package/mold/mold.mk
@@ -3,8 +3,8 @@
 # mold
 #
 ################################################################################
-# Version: Commits on Mar 07, 2025
-MOLD_VERSION = v2.37.1
+# Version: Commits on Apr 27, 2025
+MOLD_VERSION = v2.38.0
 MOLD_SITE = $(call github,rui314,mold,$(MOLD_VERSION))
 MOLD_LICENSE = MIT
 MOLD_DEPENDENCIES = zlib $(TARGET_NLS_DEPENDENCIES) tbb
