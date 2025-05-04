a7564dce593eb3731e7cc8158cceaf38f6048ae6
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-05-04 23:25:51 +0100

    update python-pycares version 4.8.0

--- package/python-pycares/python-pycares.mk
+++ package/python-pycares/python-pycares.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYCARES_VERSION = 4.7.0
+PYTHON_PYCARES_VERSION = 4.8.0
 PYTHON_PYCARES_SOURCE = pycares-$(PYTHON_PYCARES_VERSION).tar.gz
-PYTHON_PYCARES_SITE = https://files.pythonhosted.org/packages/9d/cd/dabe7fb5fd0089a1a37ae94e30b2fb094bff098492f1fbdfd8e2969d69a6
+PYTHON_PYCARES_SITE = https://files.pythonhosted.org/packages/19/7a/01ef7ce35fc1312d6c1c07f3b87f329ad6daf41bb9cd57c8f017e0b653fa
 PYTHON_PYCARES_SETUP_TYPE = setuptools
 PYTHON_PYCARES_LICENSE = MIT
 PYTHON_PYCARES_LICENSE_FILES = LICENSE
