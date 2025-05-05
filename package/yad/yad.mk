afc813e3c95b8677e797221c678cfd52269e3cf9
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-05-05 09:05:09 +0100

    use offical yad package

--- package/yad/yad.mk
+++ package/yad/yad.mk
@@ -5,11 +5,11 @@
 ################################################################################
 
 YAD_VERSION = 14.1
-YAD_SITE = $(call github,v1cont,yad,v$(YAD_VERSION))
+YAD_SOURCE = yad-$(YAD_VERSION).tar.xz
+YAD_SITE = https://github.com/v1cont/yad/releases/download/v$(YAD_VERSION)
 YAD_LICENSE = GPL-3.0
 YAD_LICENSE_FILES = COPYING
 YAD_DEPENDENCIES = host-intltool host-pkgconf $(TARGET_NLS_DEPENDENCIES)
-YAD_AUTORECONF = YES
 YAD_CONF_ENV = LIBS=$(TARGET_NLS_LIBS)
 
 YAD_CONF_OPTS = --enable-html=no
