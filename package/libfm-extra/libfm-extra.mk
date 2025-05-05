7bfe10ea3fdfea2b55a9bb6f43ea2fefb1a14164
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-05-05 08:11:54 +0100

    upate autoconfig

--- package/libfm-extra/libfm-extra.mk
+++ package/libfm-extra/libfm-extra.mk
@@ -10,12 +10,7 @@ LIBFM_EXTRA_DEPENDENCIES = libglib2 host-intltool
 LIBFM_EXTRA_LICENSE = GPL-2.0+, LGPL-2.1+
 LIBFM_EXTRA_LICENSE_FILES = COPYING src/extra/fm-xml-file.c
 LIBFM_EXTRA_INSTALL_STAGING = YES
-LIBFM_EXTRA_AUTORECONF = YES
 LIBFM_EXTRA_CONF_OPTS = --with-extra-only --with-gtk=no
-
-define LIBFM_EXTRA_RUN_AUTOGEN
-	cd $(@D) && mkdir m4 && PATH=$(BR_PATH) gtkdocize --copy
-endef
-LIBFM_EXTRA_PRE_PATCH_HOOKS += LIBFM_EXTRA_RUN_AUTOGEN
+LIBFM_EXTRA_LIBTOOL_PATCH = NO
 
 $(eval $(autotools-package))
--- package/libfm/libfm.mk
+++ package/libfm/libfm.mk
@@ -10,7 +10,7 @@ LIBFM_DEPENDENCIES = menu-cache libglib2 cairo
 LIBFM_LICENSE = GPL-2.0+, LGPL-2.1+
 LIBFM_LICENSE_FILES = COPYING src/extra/fm-xml-file.c
 LIBFM_INSTALL_STAGING = YES
-LIBFM_AUTORECONF = YES
+LIBFM_LIBTOOL_PATCH = NO
 
 ifeq ($(BR2_PACKAGE_LIBEXIF),y)
 LIBFM_CONF_OPTS += --enable-exif
@@ -26,10 +26,5 @@ else
 LIBFM_CONF_OPTS += --with-gtk=2
 LIBFM_DEPENDENCIES += libgtk2
 endif
-
-define LIBFM_RUN_AUTOGEN
-	cd $(@D) && mkdir m4 && PATH=$(BR_PATH) gtkdocize --copy
-endef
-LIBFM_PRE_PATCH_HOOKS += LIBFM_EXTRA_RUN_AUTOGEN
 
 $(eval $(autotools-package))
