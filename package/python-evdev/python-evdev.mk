1ed0f7733d18b8dfe408556cae2b4d03d5c092ed
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-05-11 13:19:54 +0100

    update python-evdev version 1.9.2

--- package/python-evdev/python-evdev.mk
+++ package/python-evdev/python-evdev.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_EVDEV_VERSION = 1.8.0
+PYTHON_EVDEV_VERSION = 1.9.2
 PYTHON_EVDEV_SOURCE = evdev-$(PYTHON_EVDEV_VERSION).tar.gz
-PYTHON_EVDEV_SITE = https://files.pythonhosted.org/packages/4c/eb/b8900082a13a47bb9f69f415174d03cbf12ce95d07345722e1f4ef0e2093
+PYTHON_EVDEV_SITE = https://files.pythonhosted.org/packages/63/fe/a17c106a1f4061ce83f04d14bcedcfb2c38c7793ea56bfb906a6fadae8cb
 PYTHON_EVDEV_SETUP_TYPE = setuptools
 PYTHON_EVDEV_LICENSE = Revised BSD License
 PYTHON_EVDEV_LICENSE_FILES = LICENSE
