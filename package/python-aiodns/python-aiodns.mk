dfd9cf88c8a20250a72568c7665fc875eb1d84bd
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-05-11 10:18:17 +0100

    update python-aiodns version 3.4.0

--- package/python-aiodns/python-aiodns.mk
+++ package/python-aiodns/python-aiodns.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_AIODNS_VERSION = 3.3.0
+PYTHON_AIODNS_VERSION = 3.4.0
 PYTHON_AIODNS_SOURCE = aiodns-$(PYTHON_AIODNS_VERSION).tar.gz
-PYTHON_AIODNS_SITE = https://files.pythonhosted.org/packages/f6/30/2c10dc401091beffef6f516da38f7e6391d2a77afe5e092956855e1d07a7
+PYTHON_AIODNS_SITE = https://files.pythonhosted.org/packages/92/9b/e96226eed7568ddfd075b03695e3e1298d9de48724128a3a2957f5ee6ec8
 PYTHON_AIODNS_SETUP_TYPE = setuptools
 PYTHON_AIODNS_LICENSE = MIT
 PYTHON_AIODNS_LICENSE_FILES = LICENSE
