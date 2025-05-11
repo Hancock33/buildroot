3e39ac5d06c837ec210bec96f6731cf683d7bfc3
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-05-11 10:20:20 +0100

    update python-trove-classifiers version 2025.5.9.12

--- package/python-trove-classifiers/python-trove-classifiers.mk
+++ package/python-trove-classifiers/python-trove-classifiers.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_TROVE_CLASSIFIERS_VERSION = 2025.5.8.15
+PYTHON_TROVE_CLASSIFIERS_VERSION = 2025.5.9.12
 PYTHON_TROVE_CLASSIFIERS_SOURCE = trove_classifiers-$(PYTHON_TROVE_CLASSIFIERS_VERSION).tar.gz
-PYTHON_TROVE_CLASSIFIERS_SITE = https://files.pythonhosted.org/packages/6f/1d/ba00fcbd5d52b2369a5a1dec07d5d7b646be4ef8234fa7f9dd763c5bca57
+PYTHON_TROVE_CLASSIFIERS_SITE = https://files.pythonhosted.org/packages/38/04/1cd43f72c241fedcf0d9a18d0783953ee301eac9e5d9db1df0f0f089d9af
 PYTHON_TROVE_CLASSIFIERS_SETUP_TYPE = setuptools
 PYTHON_TROVE_CLASSIFIERS_LICENSE = Apache-2.0
 PYTHON_TROVE_CLASSIFIERS_LICENSE_FILES = LICENSE
