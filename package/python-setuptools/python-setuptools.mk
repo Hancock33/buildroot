ec929fcbdf8b73958facaf62fc179cce9bfd1da2
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-05-15 19:37:18 +0100

    Revert "Reapply "update python-setuptools version 80.7.1""
    This reverts commit 0fb3d541d25722c266fbe0d7256e647a503c70b1.

--- package/python-setuptools/python-setuptools.mk
+++ package/python-setuptools/python-setuptools.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SETUPTOOLS_VERSION = 80.7.1
+PYTHON_SETUPTOOLS_VERSION = 80.4.0
 PYTHON_SETUPTOOLS_SOURCE = setuptools-$(PYTHON_SETUPTOOLS_VERSION).tar.gz
-PYTHON_SETUPTOOLS_SITE = https://files.pythonhosted.org/packages/9e/8b/dc1773e8e5d07fd27c1632c45c1de856ac3dbf09c0147f782ca6d990cf15
+PYTHON_SETUPTOOLS_SITE = https://files.pythonhosted.org/packages/95/32/0cc40fe41fd2adb80a2f388987f4f8db3c866c69e33e0b4c8b093fdf700e
 PYTHON_SETUPTOOLS_LICENSE = MIT
 PYTHON_SETUPTOOLS_LICENSE_FILES = LICENSE
 PYTHON_SETUPTOOLS_CPE_ID_VENDOR = python
