cb7a7faf33cdc8014545b5b4700ba27d19b70d39
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-05-26 11:32:08 +0100

    Update mold.mk
    mold 2.40.0 is a new release of the high-speed linker. It includes the following new features and bug fixes.
    
    ## New Features
    
    - mold now lays out DWARF32 debug info before DWARF64 in output debug sections to mitigate relocation overflow issues with DWARF32 when a debug info section exceeds 4 GiB. This should help people who are building extremely large executables in debug mode. (19a1bc651fa43d3357d8e597140d788811ff177d, 159ce3ba6767b59a77140e34f101e595b7864a79)
    
      Here are the details: By default, GCC and Clang emit DWARF32 even for 64-bit code. That is, the debug info typically uses 32 bit offsets to refer to locations in other debug info sections while it uses 64 bits to represent addresses. This imposes a limitation on the largest offset DWARF32 debug info can refer to, which is 4 GiB. If the output debug section exceeds that size, the linker may report a relocation overflow error. You can instruct the compilers to emit DWARF64, which uses 64 bits for inter-debug info references, if you are building an extremely large executable. So, the proper fix for the relocation overflow issue is to build all object files with `-gdwarf64`. However, rebuilding all static libraries with the new compiler flag is not always feasible for various reasons. This new feature mitigates the issue by placing DWARF32 at the beginning of output debug info sections, followed by DWARF64. By doing so, relocation overflow can be prevented as long as the total size of DWARF32 remains under 4 GiB, allowing users to continue using object files compiled without `-gdwarf64` in very large executables.
    
      Note that mold only sorts debug section contents when their size exceeds 4 GiB. Therefore, for most outputs, this mitigation doesn't change the result at all.
    
    ## Bug Fixes and Compatibility Improvements
    
    - Fixed a regression introduced in 2.38.0 in which a thread-local variable with an unusually large alignment might not have been aligned properly. That caused mislinking of systemd when LTO was enabled (#1463). (53c175850350ba24e264ffc3ac7979892b22bf4c)
    
    - Fixed a regression introduced in 2.38.0 in which `--as-needed` was ignored when creating an executable under a rare condition. (af36625a02957c9dc717954b8fbb5c28e1d7ac0e)
    
    - Fixed an assertion failure on some targets that is triggered when an weak undefined symbol in an executable is promoted to a dynamic symbol with the `-z dynamic-undefined-weak` option. (0fdffad57fa12210859724ac8ca43153755f08e5)
    
    - mold now ignores `--dynamic-linker` if `-static` is given. The new behavior is compatible with GNU ld. (c13ecc99ebce4686252ca56fcba0f8c67d8a90ac)
    
    ## Acknowledgements
    
    mold is an open-source project, and we accept donations via [GitHub Sponsors](https://github.com/sponsors/rui314) and [OpenCollective](https://opencollective.com/mold-linker). We thank everyone who sponsors our project. In particular, we would like to acknowledge the following organizations and people who have sponsored $32/month or more during this release cycle:
    
    - [Cybozu, Inc.](https://github.com/cybozu-sponsorship)
    
    - [Signal Slot Inc.](https://github.com/signal-slot)
    
    - [Mercury](https://github.com/MercuryTechnologiesSponsorships)
    
    - [G-Research-OSS](https://github.com/G-Research-OSS)
    
    - [Jinkyu Yi](https://github.com/jincreator)
    
    - [kraptor23](https://github.com/kraptor23)
    
    - [Pedro Navarro](https://github.com/pedronavf)
    
    - [Mercedes-Benz Group](https://github.com/mercedes-benz)
    
    - [Wei Wu](https://github.com/lazyparser)
    
    - [daquexian](https://github.com/daquexian)
    
    - [CubeSoft, Inc.](https://github.com/cube-soft)
    
    - [Robey Pointer](https://github.com/robey)
    
    - [nomeaning](https://github.com/nomeaning777)
    
    - [Igor Matuszewski](https://github.com/Xanewok)
    
    - [Bach Le](https://github.com/bullno1)
    
    - [Josh Triplett](https://github.com/joshtriplett)
    
    - [Martin LiÅ¡ka](https://github.com/marxin),

--- package/mold/mold.mk
+++ package/mold/mold.mk
@@ -3,8 +3,8 @@
 # mold
 #
 ################################################################################
-# Version: Commits on May 12, 2025
-MOLD_VERSION = v2.39.1
+# Version: Commits on May 26, 2025
+MOLD_VERSION = v2.40.0
 MOLD_SITE = $(call github,rui314,mold,$(MOLD_VERSION))
 MOLD_LICENSE = MIT
 MOLD_DEPENDENCIES = zlib $(TARGET_NLS_DEPENDENCIES) tbb
