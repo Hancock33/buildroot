7868301aea688bfdaedb9b9a423a28f11362d684
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-06-07 16:18:23 +0100

    update python-aiohttp version 3.12.10

--- package/python-aiohttp/python-aiohttp.mk
+++ package/python-aiohttp/python-aiohttp.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_AIOHTTP_VERSION = 3.12.9
+PYTHON_AIOHTTP_VERSION = 3.12.10
 PYTHON_AIOHTTP_SOURCE = aiohttp-$(PYTHON_AIOHTTP_VERSION).tar.gz
-PYTHON_AIOHTTP_SITE = https://files.pythonhosted.org/packages/4b/ad/5b0f3451c2275af09966f1d7c0965facd4729a5b7efdc2eb728654679f85
+PYTHON_AIOHTTP_SITE = https://files.pythonhosted.org/packages/fc/76/cc6f37a12372dd72891dad5ffc3fc71375c2f92bb4a59f7ac11119332559
 PYTHON_AIOHTTP_SETUP_TYPE = setuptools
 PYTHON_AIOHTTP_DEPENDENCIES = host-python-pkgconfig
 PYTHON_AIOHTTP_LICENSE = Apache-2.0
