ce574edc5762e6dbc70ff3faf08c1391e882e773
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-06-11 06:51:31 +0100

    update python-frozenlist version 1.7.0

--- package/python-frozenlist/python-frozenlist.mk
+++ package/python-frozenlist/python-frozenlist.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_FROZENLIST_VERSION = 1.6.2
+PYTHON_FROZENLIST_VERSION = 1.7.0
 PYTHON_FROZENLIST_SOURCE = frozenlist-$(PYTHON_FROZENLIST_VERSION).tar.gz
-PYTHON_FROZENLIST_SITE = https://files.pythonhosted.org/packages/5b/bf/a812e2fe6cb3f6c6cfc8d0303bf1742f2286004e5ec41ac8c89cf68cdb54
+PYTHON_FROZENLIST_SITE = https://files.pythonhosted.org/packages/79/b1/b64018016eeb087db503b038296fd782586432b9c077fc5c7839e9cb6ef6
 PYTHON_FROZENLIST_SETUP_TYPE = pep517
 PYTHON_FROZENLIST_LICENSE = Apache-2.0
 PYTHON_FROZENLIST_LICENSE_FILES = LICENSE
