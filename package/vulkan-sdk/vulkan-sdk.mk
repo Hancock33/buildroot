e66bf6595cfc7cf9f4c8004ede92260359ac5227
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-06-22 18:59:13 +0100

    Update vulkan-sdk.mk

--- package/vulkan-sdk/vulkan-sdk.mk
+++ package/vulkan-sdk/vulkan-sdk.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-VULKAN_SDK_VERSION = 1.4.313.0
-VULKAN_SDK_SITE = https://github.com/zeux/volk/archive/refs/tags
+VULKAN_SDK_VERSION = $(VULKAN_HEADERS_VERSION)
+VULKAN_SDK_SITE = $(call github,zeux,sdk,$(VULKAN_SDK_VERSION))
 VULKAN_SDK_LICENSE = MIT
 VULKAN_SDK_LICENSE_FILES = LICENSE.md
 VULKAN_SDK_INSTALL_STAGING = YES
 
-VULKAN_SDK_DEPENDENCIES = vulkan-headers
+VULKAN_SDK_DEPENDENCIES = vulkan-headers vulkan-loader
 
 VULKAN_SDK_CONF_OPTS += -DVOLK_INSTALL=ON
 
