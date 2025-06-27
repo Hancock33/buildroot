1aaf6fe38ef5f1e038590bf0c7dd281d7e994275
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-06-27 17:55:57 +0100

    update buildtools

--- package/binutils/binutils.mk
+++ package/binutils/binutils.mk
@@ -23,7 +23,7 @@ endif
 
 ifeq ($(BR2_BINUTILS_VERSION_GIT),y)
 #git describe --abbrev=40 origin/binutils-2_44-branch | cut -d '-' -f 2-
-	BINUTILS_VERSION = 2_44-156-g1cf55070eb071b3659794441e0b70daf5415d967
+	BINUTILS_VERSION = 2_44-163-ga0a8846f8ce2f26bb1cd3be18dfd121606852482
 	BINUTILS_SITE = $(call github,bminor,binutils-gdb,$(BINUTILS_VERSION))
 	BINUTILS_SOURCE = binutils-$(BINUTILS_VERSION).tar.gz
 	BINUTILS_FROM_GIT = y
