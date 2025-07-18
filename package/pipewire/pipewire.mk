5c0ea1ab5de8178ef67d75343276d4e1e6e00032
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-07-18 18:50:24 +0100

    build tools

--- package/pipewire/pipewire.mk
+++ package/pipewire/pipewire.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PIPEWIRE_VERSION = 1.4.6-9-g7cd256d14c5e7f3469801be515589a8eae28ebbd
+PIPEWIRE_VERSION = 1.4.6-10-g83fa53ba6e35f224ea3ff58848c3cdaed38984f5
 PIPEWIRE_SITE = $(call github,PipeWire,pipewire,$(PIPEWIRE_VERSION))
 PIPEWIRE_LICENSE = MIT, LGPL-2.1+ (libspa-alsa), GPL-2.0 (libjackserver)
 PIPEWIRE_LICENSE_FILES = COPYING LICENSE
--- package/sdl2/sdl2.mk
+++ package/sdl2/sdl2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # git describe --tags --abbrev=40 origin/release-2.30.x | cut -d '-' -f 2-
-SDL2_VERSION = 2.32.8-12-gab9f59e59adbbcca9cc4a15966dc5616eb2475f6
+SDL2_VERSION = 2.32.8-14-ge25c61cda82421697cbed70ae9484996d0d451b5
 SDL2_SITE = $(call github,libsdl-org,SDL,$(SDL2_VERSION))
 SDL2_LICENSE = Zlib
 SDL2_LICENSE_FILES = LICENSE.txt
