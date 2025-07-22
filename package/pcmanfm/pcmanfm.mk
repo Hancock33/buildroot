7e9b1e4b21e16bd7ddeec7e65ea3f3b5d629cc00
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-07-22 21:41:01 +0100

    update pcmanfm version 1.4.0

--- package/pcmanfm/pcmanfm.mk
+++ package/pcmanfm/pcmanfm.mk
@@ -4,9 +4,8 @@
 #
 ################################################################################
 
-PCMANFM_VERSION = 1.3.2
-PCMANFM_SOURCE = pcmanfm-$(PCMANFM_VERSION).tar.xz
-PCMANFM_SITE = http://sourceforge.net/projects/pcmanfm/files
+PCMANFM_VERSION = 1.4.0
+PCMANFM_SITE = $(call github,lxde,pcmanfm,$(PCMANFM_VERSION))
 PCMANFM_DEPENDENCIES = libglib2 menu-cache libfm $(TARGET_NLS_DEPENDENCIES)
 PCMANFM_LICENSE = GPL-2.0+
 PCMANFM_LICENSE_FILES = COPYING
@@ -20,5 +19,10 @@ else
 PCMANFM_CONF_OPTS += --with-gtk=2
 PCMANFM_DEPENDENCIES += libgtk2
 endif
+
+define PCMANFM_AUTOCONFIG
+	cd $(@D) && ./autogen.sh
+endef
+PCMANFM_PRE_CONFIGURE_HOOKS += PCMANFM_AUTOCONFIG
 
 $(eval $(autotools-package))
