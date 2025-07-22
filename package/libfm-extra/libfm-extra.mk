5f1c3c82205f6e8efd53f4083e468e72842b7f35
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-07-22 21:33:39 +0100

    update menu-cache version 1.1.1

--- package/libfm-extra/libfm-extra.mk
+++ package/libfm-extra/libfm-extra.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 LIBFM_EXTRA_VERSION = 1.4.0
-LIBFM_EXTRA_SITE =  $(call github,lxde,libfm,$(LIBFM_EXTRA_VERSION))
+LIBFM_EXTRA_SITE = $(call github,lxde,libfm,$(LIBFM_EXTRA_VERSION))
 LIBFM_EXTRA_DEPENDENCIES = libglib2 host-intltool
 LIBFM_EXTRA_LICENSE = GPL-2.0+, LGPL-2.1+
 LIBFM_EXTRA_LICENSE_FILES = COPYING src/extra/fm-xml-file.c
--- package/menu-cache/menu-cache.mk
+++ package/menu-cache/menu-cache.mk
@@ -4,13 +4,16 @@
 #
 ################################################################################
 
-MENU_CACHE_VERSION_MAJOR = 1.1
-MENU_CACHE_VERSION = $(MENU_CACHE_VERSION_MAJOR).0
-MENU_CACHE_SOURCE = menu-cache-$(MENU_CACHE_VERSION).tar.xz
-MENU_CACHE_SITE = http://sourceforge.net/projects/lxde/files/menu-cache/$(MENU_CACHE_VERSION_MAJOR)
+MENU_CACHE_VERSION = 1.1.1
+MENU_CACHE_SITE = $(call github,lxde,menu-cache,$(MENU_CACHE_VERSION))
 MENU_CACHE_DEPENDENCIES = libfm-extra libglib2
 MENU_CACHE_LICENSE = LGPL-2.1
 MENU_CACHE_LICENSE_FILES = COPYING
 MENU_CACHE_INSTALL_STAGING = YES
+
+define MENU_CACHE_AUTOCONFIG
+	cd $(@D) && ./autogen.sh
+endef
+MENU_CACHE_PRE_CONFIGURE_HOOKS += MENU_CACHE_AUTOCONFIG
 
 $(eval $(autotools-package))
