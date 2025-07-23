d84a9bd1c40e999bd5a02338b5d1fd9d299fc11d
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-07-23 17:10:27 +0100

    Update pipewire.mk

--- package/pipewire/pipewire.mk
+++ package/pipewire/pipewire.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PIPEWIRE_VERSION = 1.4.6-13-g5aa46bbdddaffa185ffdc9f6dba564de2af0baa2
+PIPEWIRE_VERSION = 1.4.7
 PIPEWIRE_SITE = $(call github,PipeWire,pipewire,$(PIPEWIRE_VERSION))
 PIPEWIRE_LICENSE = MIT, LGPL-2.1+ (libspa-alsa), GPL-2.0 (libjackserver)
 PIPEWIRE_LICENSE_FILES = COPYING LICENSE
