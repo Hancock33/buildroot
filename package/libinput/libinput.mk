4d890729ed654c9d188ed221389a388c9a257df7
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-07-31 20:01:16 +0100

    update libinput version 1.29.0

--- package/libinput/libinput.mk
+++ package/libinput/libinput.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBINPUT_VERSION = 1.28.1
+LIBINPUT_VERSION = 1.29.0
 LIBINPUT_SOURCE = libinput-$(LIBINPUT_VERSION).tar.gz
 LIBINPUT_SITE = https://gitlab.freedesktop.org/libinput/libinput/-/archive/$(LIBINPUT_VERSION)
 LIBINPUT_DEPENDENCIES = host-pkgconf libevdev mtdev libudev
