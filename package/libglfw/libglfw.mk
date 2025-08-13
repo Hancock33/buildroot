8a673518f19b2c979633bb52c66f97d07a3152d4
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-08-13 23:02:23 +0100

    update libglfw version 3.5.0

--- package/libglfw/libglfw.mk
+++ package/libglfw/libglfw.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBGLFW_VERSION = 3.4
+LIBGLFW_VERSION = 3.5.0
 LIBGLFW_SITE = $(call github,glfw,glfw,$(LIBGLFW_VERSION))
 LIBGLFW_INSTALL_STAGING = YES
 LIBGLFW_LICENSE = Zlib
