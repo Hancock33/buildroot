e9b5368284ad2fc9dbc84a8164c4abc5bfcd5eca
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-08-13 22:53:54 +0100

    mass update kodi plugins

--- package/kodi-screensaver-stars/kodi-screensaver-stars.mk
+++ package/kodi-screensaver-stars/kodi-screensaver-stars.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_SCREENSAVER_STARS_VERSION = 22.0.2-Piers
+KODI_SCREENSAVER_STARS_VERSION = 22.0.3-Piers
 KODI_SCREENSAVER_STARS_SITE = $(call github,xbmc,screensaver.stars,$(KODI_SCREENSAVER_STARS_VERSION))
 KODI_SCREENSAVER_STARS_LICENSE = GPL-2.0+
 KODI_SCREENSAVER_STARS_LICENSE_FILES = LICENSE.md
