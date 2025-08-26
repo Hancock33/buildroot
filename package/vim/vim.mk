6f52bc67f4046e1a2965c50b20ee78cc08b1cd07
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-08-26 22:21:26 +0100

    update vim version 9.1.1696

--- package/vim/vim.mk
+++ package/vim/vim.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-VIM_VERSION = 9.1.1686
+VIM_VERSION = 9.1.1696
 VIM_SITE = $(call github,vim,vim,v$(VIM_VERSION))
 VIM_DEPENDENCIES = ncurses $(TARGET_NLS_DEPENDENCIES)
 VIM_SUBDIR = src
@@ -18,7 +18,8 @@ VIM_CONF_ENV = \
 	vim_cv_stat_ignores_slash=yes \
 	vim_cv_memmove_handles_overlap=yes \
 	ac_cv_sizeof_int=4 \
-	ac_cv_small_wchar_t=no
+	ac_cv_small_wchar_t=no \
+	LIBS="-ltinfo"
 # GUI/X11 headers leak from the host so forcibly disable them
 VIM_CONF_OPTS = --with-tlib=ncurses --enable-gui=no --without-x
 VIM_LICENSE = Charityware
@@ -49,7 +50,6 @@ endif
 define VIM_INSTALL_TARGET_CMDS
 	cd $(@D)/src; \
 		$(TARGET_MAKE_ENV) $(MAKE) DESTDIR=$(TARGET_DIR) installvimbin; \
-		$(TARGET_MAKE_ENV) $(MAKE) DESTDIR=$(TARGET_DIR) installpack; \
 		$(TARGET_MAKE_ENV) $(MAKE) DESTDIR=$(TARGET_DIR) installtools; \
 		$(TARGET_MAKE_ENV) $(MAKE) DESTDIR=$(TARGET_DIR) installlinks
 endef
