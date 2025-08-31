f5869825da54f098e8f336f85ec6bd40780506b3
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-08-31 18:00:02 +0100

    buildtools

--- package/mesa3d/mesa3d.mk
+++ package/mesa3d/mesa3d.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # git describe --abbrev=40 origin/staging/25.1 | cut -d '-' -f 2-
-MESA3D_VERSION = 25.2.1-76-g79d0f513b46c078957133da82a4fc6952cef2a6a
+MESA3D_VERSION = 25.2.1-89-g74268b10d3b57f5c7c5b8d36b83bed3126e5b9e9
 MESA3D_SITE = $(call gitlabfreedesktop,mesa,mesa,$(MESA3D_VERSION))
 MESA3D_LICENSE = MIT, SGI, Khronos
 MESA3D_LICENSE_FILES = \
--- package/sdl2/sdl2.mk
+++ package/sdl2/sdl2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # git describe --tags --abbrev=40 origin/release-2.30.x | cut -d '-' -f 2-
-SDL2_VERSION = 2.32.8-26-g54772f345ab1ce1c13e8a7a1fb9f56bde045036c
+SDL2_VERSION = 2.32.8-31-gc8e984701eb29cdbd91eff145624d8120cff921f
 SDL2_SITE = $(call github,libsdl-org,SDL,$(SDL2_VERSION))
 SDL2_LICENSE = Zlib
 SDL2_LICENSE_FILES = LICENSE.txt
