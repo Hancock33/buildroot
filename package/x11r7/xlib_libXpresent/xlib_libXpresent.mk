f4738fe7acb771e990a25a4d1fa691c6aa596a70
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-09-08 07:20:54 +0100

    update xlib_libXpresent version 1.0.2

--- package/x11r7/xlib_libXpresent/xlib_libXpresent.mk
+++ package/x11r7/xlib_libXpresent/xlib_libXpresent.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XLIB_LIBXPRESENT_VERSION = 1.0.1
+XLIB_LIBXPRESENT_VERSION = 1.0.2
 XLIB_LIBXPRESENT_SOURCE = libXpresent-$(XLIB_LIBXPRESENT_VERSION).tar.xz
 XLIB_LIBXPRESENT_SITE = http://xorg.freedesktop.org/releases/individual/lib
 XLIB_LIBXPRESENT_LICENSE = MIT
