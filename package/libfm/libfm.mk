3768fab9e4c50451bdf840484bcdf00e0f3c3d88
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-09-11 21:34:12 +0100

    drop libgtk2 from libfm & pcmanfm

--- package/libfm/libfm.mk
+++ package/libfm/libfm.mk
@@ -6,25 +6,18 @@
 
 LIBFM_VERSION = 1.4.0
 LIBFM_SITE =  $(call github,lxde,libfm,$(LIBFM_VERSION))
-LIBFM_DEPENDENCIES = menu-cache libglib2 cairo
+LIBFM_DEPENDENCIES = menu-cache libglib2 cairo libgtk3
 LIBFM_LICENSE = GPL-2.0+, LGPL-2.1+
 LIBFM_LICENSE_FILES = COPYING src/extra/fm-xml-file.c
 LIBFM_INSTALL_STAGING = YES
 LIBFM_LIBTOOL_PATCH = NO
+LIBFM_CONF_OPTS = --with-gtk=3
 
 ifeq ($(BR2_PACKAGE_LIBEXIF),y)
 LIBFM_CONF_OPTS += --enable-exif
 LIBFM_DEPENDENCIES += libexif
 else
 LIBFM_CONF_OPTS += --disable-exif
-endif
-
-ifeq ($(BR2_PACKAGE_LIBGTK3_X11),y)
-LIBFM_CONF_OPTS += --with-gtk=3
-LIBFM_DEPENDENCIES += libgtk3
-else
-LIBFM_CONF_OPTS += --with-gtk=2
-LIBFM_DEPENDENCIES += libgtk2
 endif
 
 $(eval $(autotools-package))
