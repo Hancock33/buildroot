2f3b563d9aa17031200ee582c75001a96dac3426
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-09-16 19:36:49 +0100

    update inotify-tools version 4.25.9.0

--- package/inotify-tools/inotify-tools.mk
+++ package/inotify-tools/inotify-tools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-INOTIFY_TOOLS_VERSION = 4.23.9.0
+INOTIFY_TOOLS_VERSION = 4.25.9.0
 INOTIFY_TOOLS_SITE = $(call github,inotify-tools,inotify-tools,$(INOTIFY_TOOLS_VERSION))
 INOTIFY_TOOLS_LICENSE = GPL-2.0+
 INOTIFY_TOOLS_LICENSE_FILES = COPYING
