0c82256b03f28a3e27dcf037c548a30ebd1e72c7
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-09-18 22:25:42 +0100

    update python-pyopenssl version 25.3.0

--- package/python-pyopenssl/python-pyopenssl.mk
+++ package/python-pyopenssl/python-pyopenssl.mk
@@ -4,13 +4,11 @@
 #
 ################################################################################
 
-PYTHON_PYOPENSSL_VERSION = 25.2.0
+PYTHON_PYOPENSSL_VERSION = 25.3.0
 PYTHON_PYOPENSSL_SOURCE = pyopenssl-$(PYTHON_PYOPENSSL_VERSION).tar.gz
-PYTHON_PYOPENSSL_SITE = https://files.pythonhosted.org/packages/fc/d7/5b1410932bfaf0bbf96426a8e08472ed26f2052c353f2dea6e6636850545
+PYTHON_PYOPENSSL_SITE = https://files.pythonhosted.org/packages/80/be/97b83a464498a79103036bc74d1038df4a7ef0e402cfaf4d5e113fb14759
+PYTHON_PYOPENSSL_SETUP_TYPE = setuptools
 PYTHON_PYOPENSSL_LICENSE = Apache-2.0
 PYTHON_PYOPENSSL_LICENSE_FILES = LICENSE
-PYTHON_PYOPENSSL_CPE_ID_VENDOR = pyopenssl
-PYTHON_PYOPENSSL_CPE_ID_PRODUCT = pyopenssl
-PYTHON_PYOPENSSL_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
