551f40ba4c0826e63dc8ff0fd1464a6a4592a713
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-09-26 17:23:55 +0100

    Update vlc.mk

--- package/vlc/vlc.mk
+++ package/vlc/vlc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version: Commits on Sept 12, 2025
-VLC_VERSION = 3.0.22-rc1-35-g9e23cc7e00954f91607471fd0f1c109a715650b7
+VLC_VERSION = 3.0.22-rc1-49-g8052b3a827571387dae28667c4916aefc76309a0
 VLC_SITE = https://code.videolan.org/videolan/vlc.git
 VLC_SITE_METHOD = git
 #VLC_SOURCE = vlc-$(VLC_VERSION).tar.xz
