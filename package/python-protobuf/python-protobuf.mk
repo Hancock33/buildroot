f9543ba66b34a4c11af16d7aa1fd2a1a7d39d3d4
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-10-16 22:15:39 +0100

    update protobuf version 33.0

--- package/python-protobuf/python-protobuf.mk
+++ package/python-protobuf/python-protobuf.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PROTOBUF_VERSION = 6.32.1
+PYTHON_PROTOBUF_VERSION = 6.33.0
 PYTHON_PROTOBUF_SOURCE = protobuf-$(PYTHON_PROTOBUF_VERSION).tar.gz
-PYTHON_PROTOBUF_SITE = https://files.pythonhosted.org/packages/fa/a4/cc17347aa2897568beece2e674674359f911d6fe21b0b8d6268cd42727ac
+PYTHON_PROTOBUF_SITE = https://files.pythonhosted.org/packages/19/ff/64a6c8f420818bb873713988ca5492cba3a7946be57e027ac63495157d97
 PYTHON_PROTOBUF_LICENSE = BSD-3-Clause
 PYTHON_PROTOBUF_LICENSE_FILES = LICENSE
 PYTHON_PROTOBUF_DEPENDENCIES = host-protobuf
