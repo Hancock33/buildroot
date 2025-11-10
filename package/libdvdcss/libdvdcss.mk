6361ce42f09d9381e184253b97a1f531b210ca6e
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-11-10 20:17:13 +0000

    update libdvdcss version 1.5.0

--- package/libdvdcss/libdvdcss.mk
+++ package/libdvdcss/libdvdcss.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-LIBDVDCSS_VERSION = 1.4.3
-LIBDVDCSS_SOURCE = libdvdcss-$(LIBDVDCSS_VERSION).tar.bz2
+LIBDVDCSS_VERSION = 1.5.0
+LIBDVDCSS_SOURCE = libdvdcss-$(LIBDVDCSS_VERSION).tar.xz
 LIBDVDCSS_SITE = http://www.videolan.org/pub/videolan/libdvdcss/$(LIBDVDCSS_VERSION)
 LIBDVDCSS_INSTALL_STAGING = YES
 LIBDVDCSS_LICENSE = GPL-2.0+
