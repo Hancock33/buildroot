e8f3b35395dbb4bf543e23a7314cb6c2162eae75
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-12-06 08:10:23 +0000

    update xterm version 404

--- package/xterm/xterm.mk
+++ package/xterm/xterm.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XTERM_VERSION = 403
+XTERM_VERSION = 404
 XTERM_SOURCE = xterm-$(XTERM_VERSION).tgz
 XTERM_SITE = http://invisible-mirror.net/archives/xterm
 XTERM_DEPENDENCIES = ncurses xlib_libXaw host-pkgconf
