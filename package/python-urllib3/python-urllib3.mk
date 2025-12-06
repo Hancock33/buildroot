3380d651db2aa57584e465656b8f19c8c8948ddf
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-12-06 08:17:10 +0000

    update python-urllib3 version 2.6.0

--- package/python-urllib3/python-urllib3.mk
+++ package/python-urllib3/python-urllib3.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_URLLIB3_VERSION = 2.5.0
+PYTHON_URLLIB3_VERSION = 2.6.0
 PYTHON_URLLIB3_SOURCE = urllib3-$(PYTHON_URLLIB3_VERSION).tar.gz
-PYTHON_URLLIB3_SITE = https://files.pythonhosted.org/packages/15/22/9ee70a2574a4f4599c47dd506532914ce044817c7752a79b6a51286319bc
+PYTHON_URLLIB3_SITE = https://files.pythonhosted.org/packages/1c/43/554c2569b62f49350597348fc3ac70f786e3c32e7f19d266e19817812dd3
 PYTHON_URLLIB3_LICENSE = MIT
 PYTHON_URLLIB3_LICENSE_FILES = LICENSE.txt
 PYTHON_URLLIB3_CPE_ID_VENDOR = python
