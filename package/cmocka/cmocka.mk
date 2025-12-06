23489f6c8b6ac33141d228a6d9a977855258a8f1
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-12-06 08:25:26 +0000

    update cmocka version 2.0.0

--- package/cmocka/cmocka.mk
+++ package/cmocka/cmocka.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-CMOCKA_VERSION = 1.1.8
+CMOCKA_VERSION = 2.0.0
 CMOCKA_SOURCE = cmocka-$(CMOCKA_VERSION).tar.xz
-CMOCKA_SITE = https://cmocka.org/files/1.1
+CMOCKA_SITE = https://cmocka.org/files/2.0
 CMOCKA_LICENSE = Apache-2.0
 CMOCKA_LICENSE_FILES = COPYING
 CMOCKA_INSTALL_STAGING = YES
