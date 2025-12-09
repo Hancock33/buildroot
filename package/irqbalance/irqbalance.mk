0e69511e132e9afbf76daac7aa2538d897f62482
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-12-09 19:27:12 +0000

    update irqbalance version 1.9.5

--- package/irqbalance/irqbalance.mk
+++ package/irqbalance/irqbalance.mk
@@ -4,47 +4,44 @@
 #
 ################################################################################
 
-IRQBALANCE_VERSION = 1.9.4
+IRQBALANCE_VERSION = 1.9.5
 IRQBALANCE_SITE = $(call github,irqbalance,irqbalance,v$(IRQBALANCE_VERSION))
 IRQBALANCE_LICENSE = GPL-2.0
 IRQBALANCE_LICENSE_FILES = COPYING
 IRQBALANCE_SELINUX_MODULES = irqbalance
 IRQBALANCE_DEPENDENCIES = host-pkgconf libglib2
-# Autoreconf needed because package is distributed without a configure script
-IRQBALANCE_AUTORECONF = YES
-
-# GCC 15 defaults to `-std=gnu23` which breaks compilation
-IRQBALANCE_CFLAGS += -std=gnu17
 
 IRQBALANCE_CONF_ENV += \
 	CFLAGS="$(IRQBALANCE_CFLAGS)"
 
 ifeq ($(BR2_PACKAGE_LIBCAP_NG),y)
 IRQBALANCE_DEPENDENCIES += libcap-ng
-IRQBALANCE_CONF_OPTS += --with-libcap-ng
+IRQBALANCE_CONF_OPTS += -Dcapng=enabled
 else
-IRQBALANCE_CONF_OPTS += --without-libcap-ng
+IRQBALANCE_CONF_OPTS += -Dcapng=disabled
 endif
 
 ifeq ($(BR2_PACKAGE_NCURSES_WCHAR),y)
 IRQBALANCE_DEPENDENCIES += ncurses
-IRQBALANCE_CONF_OPTS += --with-irqbalance-ui
+IRQBALANCE_CONF_OPTS += -Dui=enabled
 else
-IRQBALANCE_CONF_OPTS += --without-irqbalance-ui
+IRQBALANCE_CONF_OPTS += -Dui=disabled
 endif
 
 ifeq ($(BR2_PACKAGE_NUMACTL),y)
 IRQBALANCE_DEPENDENCIES += numactl
-IRQBALANCE_CONF_OPTS += --enable-numa
+IRQBALANCE_CONF_OPTS += -Dnuma=enabled
 else
-IRQBALANCE_CONF_OPTS += --disable-numa
+IRQBALANCE_CONF_OPTS += -Dnuma=disabled
 endif
 
 ifeq ($(BR2_PACKAGE_SYSTEMD),y)
 IRQBALANCE_DEPENDENCIES += systemd
-IRQBALANCE_CONF_OPTS += --with-systemd
+IRQBALANCE_CONF_OPTS += -Dsystemd=enabled
+IRQBALANCE_CONF_OPTS += -Dsystemd-service=false
 else
-IRQBALANCE_CONF_OPTS += --without-systemd
+IRQBALANCE_CONF_OPTS += -Dsystemd=disabled
+IRQBALANCE_CONF_OPTS += -Dsystemd-service=false
 endif
 
 define IRQBALANCE_INSTALL_INIT_SYSV
@@ -57,4 +54,4 @@ define IRQBALANCE_INSTALL_INIT_SYSTEMD
 		$(TARGET_DIR)/usr/lib/systemd/system/irqbalance.service
 endef
 
-$(eval $(autotools-package))
+$(eval $(meson-package))
