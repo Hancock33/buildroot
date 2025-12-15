594887bb20168ce11fec9daa2cf445f7c383a11c
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-12-15 20:09:14 +0000

    update libfm-extra version 1.4.1

--- package/libfm-extra/libfm-extra.mk
+++ package/libfm-extra/libfm-extra.mk
@@ -4,13 +4,17 @@
 #
 ################################################################################
 
-LIBFM_EXTRA_VERSION = 1.4.0
+LIBFM_EXTRA_VERSION = 1.4.1
 LIBFM_EXTRA_SITE = $(call github,lxde,libfm,$(LIBFM_EXTRA_VERSION))
 LIBFM_EXTRA_DEPENDENCIES = libglib2 host-intltool
 LIBFM_EXTRA_LICENSE = GPL-2.0+, LGPL-2.1+
 LIBFM_EXTRA_LICENSE_FILES = COPYING src/extra/fm-xml-file.c
 LIBFM_EXTRA_INSTALL_STAGING = YES
 LIBFM_EXTRA_CONF_OPTS = --with-extra-only --with-gtk=no
-LIBFM_EXTRA_LIBTOOL_PATCH = NO
+
+define LIBFM_EXTRA_AUTOCONFIG
+	cd $(@D) && PATH=/usr/bin autoreconf -f -i
+endef
+LIBFM_EXTRA_POST_PATCH_HOOKS += LIBFM_EXTRA_AUTOCONFIG
 
 $(eval $(autotools-package))
--- package/libfm/libfm.mk
+++ package/libfm/libfm.mk
@@ -4,13 +4,12 @@
 #
 ################################################################################
 
-LIBFM_VERSION = 1.4.0
+LIBFM_VERSION = 1.4.1
 LIBFM_SITE =  $(call github,lxde,libfm,$(LIBFM_VERSION))
 LIBFM_DEPENDENCIES = menu-cache libglib2 cairo libgtk3
 LIBFM_LICENSE = GPL-2.0+, LGPL-2.1+
 LIBFM_LICENSE_FILES = COPYING src/extra/fm-xml-file.c
 LIBFM_INSTALL_STAGING = YES
-LIBFM_LIBTOOL_PATCH = NO
 LIBFM_CONF_OPTS = --with-gtk=3
 
 ifeq ($(BR2_PACKAGE_LIBEXIF),y)
@@ -19,5 +18,10 @@ LIBFM_DEPENDENCIES += libexif
 else
 LIBFM_CONF_OPTS += --disable-exif
 endif
+
+define LIBFM_AUTOCONFIG
+	cd $(@D) && PATH=/usr/bin autoreconf -f -i
+endef
+LIBFM_POST_PATCH_HOOKS += LIBFM_AUTOCONFIG
 
 $(eval $(autotools-package))
