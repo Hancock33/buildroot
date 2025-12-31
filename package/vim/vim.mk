9457afca2a814f60e9511cfbb87f8d7002414192
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-12-31 12:27:08 +0000

    update vim version 9.1.2035

--- package/vim/vim.mk
+++ package/vim/vim.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-VIM_VERSION = 9.1.2031
+VIM_VERSION = 9.1.2035
 VIM_SITE = $(call github,vim,vim,v$(VIM_VERSION))
 VIM_DEPENDENCIES = ncurses $(TARGET_NLS_DEPENDENCIES)
 VIM_SUBDIR = src
