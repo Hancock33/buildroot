8e954ab0bac2584d6dc45a0ac84cbb22ef6f3b4d
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-01-08 07:23:18 +0000

    update python-pathspec version 1.0.2

--- package/python-pathspec/python-pathspec.mk
+++ package/python-pathspec/python-pathspec.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PATHSPEC_VERSION = 0.12.1
+PYTHON_PATHSPEC_VERSION = 1.0.2
 PYTHON_PATHSPEC_SOURCE = pathspec-$(PYTHON_PATHSPEC_VERSION).tar.gz
-PYTHON_PATHSPEC_SITE = https://files.pythonhosted.org/packages/ca/bc/f35b8446f4531a7cb215605d100cd88b7ac6f44ab3fc94870c120ab3adbf
+PYTHON_PATHSPEC_SITE = https://files.pythonhosted.org/packages/41/b9/6eb731b52f132181a9144bbe77ff82117f6b2d2fbfba49aaab2c014c4760
 PYTHON_PATHSPEC_LICENSE = MPL-2.0
 PYTHON_PATHSPEC_LICENSE_FILES = LICENSE
 PYTHON_PATHSPEC_SETUP_TYPE = flit
