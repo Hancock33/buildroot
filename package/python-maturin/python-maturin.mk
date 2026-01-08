1af6dae1d0543294fb86587cb9b471eb6be04b45
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-01-08 20:00:27 +0000

    update python-maturin version 1.11.4

--- package/python-maturin/python-maturin.mk
+++ package/python-maturin/python-maturin.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_MATURIN_VERSION = 1.11.2
+PYTHON_MATURIN_VERSION = 1.11.4
 PYTHON_MATURIN_SOURCE_PYPI = maturin-$(PYTHON_MATURIN_VERSION).tar.gz
-PYTHON_MATURIN_SITE_PYPI = https://files.pythonhosted.org/packages/2f/1c/00b48c6b93a5b3795ec96165a60dbafd3c5094aae281ba56812a8cad4fc7
+PYTHON_MATURIN_SITE_PYPI = https://files.pythonhosted.org/packages/d2/8b/d26b2e27ac41478426b367100f9bdd3ade8b4314e7aab4c9f44c789c7029
 PYTHON_MATURIN_SITE = $(PYTHON_MATURIN_SITE_PYPI)/$(PYTHON_MATURIN_SOURCE_PYPI)?buildroot-path=filename
 PYTHON_MATURIN_SETUP_TYPE = setuptools-rust
 PYTHON_MATURIN_LICENSE = Apache-2.0 or MIT
