e93c7cbcdf31d164933e172b6e5b94c24c784e6f
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-01-20 20:11:11 +0000

    update libtalloc version 2.4.4

--- package/libtalloc/libtalloc.mk
+++ package/libtalloc/libtalloc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBTALLOC_VERSION = 2.4.3
+LIBTALLOC_VERSION = 2.4.4
 LIBTALLOC_SOURCE = talloc-$(LIBTALLOC_VERSION).tar.gz
 LIBTALLOC_SITE = https://www.samba.org/ftp/talloc
 LIBTALLOC_LICENSE = LGPL-3.0+
