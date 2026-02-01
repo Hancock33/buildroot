72b866a67f1b9818ec9eec5c2207c57c698eaf4d
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-02-01 14:54:16 +0000

    Update lcms2.mk

--- package/lcms2/lcms2.mk
+++ package/lcms2/lcms2.mk
@@ -11,22 +11,22 @@ LCMS2_LICENSE_FILES = LICENSE
 LCMS2_CPE_ID_VENDOR = littlecms
 LCMS2_CPE_ID_PRODUCT = little_cms
 LCMS2_INSTALL_STAGING = YES
+
+LCMS2_CONF_OPTS = -Dtests=disabled
+
 # tiff is only used by tificc sample
-LCMS2_CONF_OPTS = --without-tiff
+LCMS2_CONF_OPTS = -Dtiff=disabled
 
 ifeq ($(BR2_PACKAGE_JPEG),y)
-LCMS2_CONF_OPTS += --with-jpeg
+LCMS2_CONF_OPTS += -Djpeg=enabled
 LCMS2_DEPENDENCIES += jpeg
 else
-LCMS2_CONF_OPTS += --without-jpeg
+LCMS2_CONF_OPTS += -Djpeg=disabled
 endif
 
 ifeq ($(BR2_PACKAGE_ZLIB),y)
-LCMS2_CONF_OPTS += --with-zlib
 LCMS2_DEPENDENCIES += zlib
-else
-LCMS2_CONF_OPTS += --without-zlib
 endif
 
-$(eval $(autotools-package))
-$(eval $(host-autotools-package))
+$(eval $(meson-package))
+$(eval $(host-meson-package))
