7bd8985dbc84a6c45ee86aa882b77668be381658
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-02-07 20:03:11 +0000

    update yaml-cpp version 0.9.0

--- package/yaml-cpp/yaml-cpp.mk
+++ package/yaml-cpp/yaml-cpp.mk
@@ -3,9 +3,10 @@
 # yaml-cpp
 #
 ################################################################################
-
-YAML_CPP_VERSION = 0.8.0
-YAML_CPP_SITE = $(call github,jbeder,yaml-cpp,$(YAML_CPP_VERSION))
+# Version: Commits on Feb 04, 2026
+YAML_CPP_VERSION = 0.9.0
+YAML_CPP_SOURCE =  yaml-cpp-0.9.0.tar.gz
+YAML_CPP_SITE = $(call github,jbeder,yaml-cpp,yaml-cpp-$(YAML_CPP_VERSION))
 YAML_CPP_INSTALL_STAGING = YES
 YAML_CPP_LICENSE = MIT
 YAML_CPP_LICENSE_FILES = LICENSE
