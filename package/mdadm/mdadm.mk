5fb4ba841e9ecd9329480375b98eb321b7b2cfd1
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-03-22 21:09:11 +0000

    update mdadm version 4.6

--- package/mdadm/mdadm.mk
+++ package/mdadm/mdadm.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-MDADM_VERSION = 4.4
-MDADM_SOURCE = mdadm-$(MDADM_VERSION).tar.xz
-MDADM_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/raid/mdadm
+MDADM_VERSION = 4.6
+MDADM_SOURCE = mdadm-$(MDADM_VERSION).tar.gz
+MDADM_SITE = https://git.kernel.org/pub/scm/utils/mdadm/mdadm.git/snapshot
 MDADM_LICENSE = GPL-2.0+
 MDADM_LICENSE_FILES = COPYING
 MDADM_CPE_ID_VALID = YES
