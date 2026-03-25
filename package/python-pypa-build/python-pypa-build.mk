49172d6317b97c2f5bd3215dce1298b36cd388a9
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-03-25 19:21:26 +0000

    update python-pypa-build version 1.4.2

--- package/python-pypa-build/python-pypa-build.mk
+++ package/python-pypa-build/python-pypa-build.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYPA_BUILD_VERSION = 1.4.0
+PYTHON_PYPA_BUILD_VERSION = 1.4.2
 PYTHON_PYPA_BUILD_SOURCE = build-$(PYTHON_PYPA_BUILD_VERSION).tar.gz
-PYTHON_PYPA_BUILD_SITE = https://files.pythonhosted.org/packages/42/18/94eaffda7b329535d91f00fe605ab1f1e5cd68b2074d03f255c7d250687d
+PYTHON_PYPA_BUILD_SITE = https://files.pythonhosted.org/packages/6c/1d/ab15c8ac57f4ee8778d7633bc6685f808ab414437b8644f555389cdc875e
 PYTHON_PYPA_BUILD_LICENSE = MIT
 PYTHON_PYPA_BUILD_LICENSE_FILES = LICENSE
 PYTHON_PYPA_BUILD_SETUP_TYPE = flit-bootstrap
