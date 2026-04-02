951a588ba3c4cbc61a1ba59cc1387655aeaa8d44
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-04-02 22:56:49 +0100

    Revert "update python-mypy version 1.20.0"
    This reverts commit 46384e92a176ced5086d3421a429348097ea1ba5.

--- package/python-mypy/python-mypy.mk
+++ package/python-mypy/python-mypy.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_MYPY_VERSION = 1.20.0
+PYTHON_MYPY_VERSION = 1.19.1
 PYTHON_MYPY_SOURCE = mypy-$(PYTHON_MYPY_VERSION).tar.gz
-PYTHON_MYPY_SITE = https://files.pythonhosted.org/packages/f8/5c/b0089fe7fef0a994ae5ee07029ced0526082c6cfaaa4c10d40a10e33b097
+PYTHON_MYPY_SITE = https://files.pythonhosted.org/packages/f5/db/4efed9504bc01309ab9c2da7e352cc223569f05478012b5d9ece38fd44d2
 PYTHON_MYPY_SETUP_TYPE = setuptools
 PYTHON_MYPY_LICENSE = Apache-2.0, BSD-2-Clause, Python-2.0.1
 PYTHON_MYPY_LICENSE_FILES = LICENSE
