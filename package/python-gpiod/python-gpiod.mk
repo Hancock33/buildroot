5fa1e3b576fd10025ce779294745949cdea752e8
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-04-06 21:07:49 +0100

    update python-gpiod version 2.4.1

--- package/python-gpiod/python-gpiod.mk
+++ package/python-gpiod/python-gpiod.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-PYTHON_GPIOD_VERSION = 2.4.0
+PYTHON_GPIOD_VERSION = 2.4.1
 PYTHON_GPIOD_SOURCE = gpiod-$(PYTHON_GPIOD_VERSION).tar.gz
-PYTHON_GPIOD_SITE = https://files.pythonhosted.org/packages/0c/dc/5a6bd309345bd9cfa7e098174ab7e65367e408539b6c1998e4f267c673cd
+PYTHON_GPIOD_SITE = https://files.pythonhosted.org/packages/2d/05/744b509dc7ddaaaa9becff6d7c2ec85653d0ae5f53c0a3ad1aaac91bb588
 PYTHON_GPIOD_SETUP_TYPE = setuptools
-PYTHON_GPIOD_LICENSE = LGPL-2.1+
+PYTHON_GPIOD_LICENSE = LGPL-2.1
 # The package license follows libgpiod's license but doesn't include the LICENSE text in the pypi distrobuted package again
-PYTHON_GPIOD_LICENSE_FILES = pyproject.toml
+PYTHON_GPIOD_LICENSE_FILES = LICENSE
 PYTHON_GPIOD_DEPENDENCIES = libgpiod2
 PYTHON_GPIOD_ENV = LINK_SYSTEM_LIBGPIOD=1
 
