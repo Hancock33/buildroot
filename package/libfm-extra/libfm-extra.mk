972ce33cd274cf3ece93df184e85e04b00c3c99a
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-04-07 18:34:24 +0100

    use AUTORECONF  for pcmanfm

--- package/libfm-extra/libfm-extra.mk
+++ package/libfm-extra/libfm-extra.mk
@@ -11,10 +11,6 @@ LIBFM_EXTRA_LICENSE = GPL-2.0+, LGPL-2.1+
 LIBFM_EXTRA_LICENSE_FILES = COPYING src/extra/fm-xml-file.c
 LIBFM_EXTRA_INSTALL_STAGING = YES
 LIBFM_EXTRA_CONF_OPTS = --with-extra-only --with-gtk=no
-
-define LIBFM_EXTRA_AUTOCONFIG
-	cd $(@D) && PATH=/usr/bin autoreconf -fiv
-endef
-LIBFM_EXTRA_POST_PATCH_HOOKS += LIBFM_EXTRA_AUTOCONFIG
+LIBFM_EXTRA_AUTORECONF = YES
 
 $(eval $(autotools-package))
--- package/libfm/libfm.mk
+++ package/libfm/libfm.mk
@@ -11,6 +11,7 @@ LIBFM_LICENSE = GPL-2.0+, LGPL-2.1+
 LIBFM_LICENSE_FILES = COPYING src/extra/fm-xml-file.c
 LIBFM_INSTALL_STAGING = YES
 LIBFM_CONF_OPTS = --with-gtk=3
+LIBFM_AUTORECONF = YES
 
 ifeq ($(BR2_PACKAGE_LIBEXIF),y)
 LIBFM_CONF_OPTS += --enable-exif
@@ -18,10 +19,5 @@ LIBFM_DEPENDENCIES += libexif
 else
 LIBFM_CONF_OPTS += --disable-exif
 endif
-
-define LIBFM_AUTOCONFIG
-	cd $(@D) && PATH=/usr/bin autoreconf -f -i
-endef
-LIBFM_POST_PATCH_HOOKS += LIBFM_AUTOCONFIG
 
 $(eval $(autotools-package))
