264efc65a02e876b0a603aae0753eb6548cb08d0
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-04-13 20:55:32 +0100

    update xterm version 408

--- package/xterm/xterm.mk
+++ package/xterm/xterm.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XTERM_VERSION = 407
+XTERM_VERSION = 408
 XTERM_SOURCE = xterm-$(XTERM_VERSION).tgz
 XTERM_SITE = http://invisible-mirror.net/archives/xterm
 XTERM_DEPENDENCIES = ncurses xlib_libXaw host-pkgconf
