9f3cb100599d21c20edd3eafc5acd5f9896d3092
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-04-18 19:50:49 +0100

    update kodi-pvr-plutotv deps

--- package/kodi-pvr-plutotv/kodi-pvr-plutotv.mk
+++ package/kodi-pvr-plutotv/kodi-pvr-plutotv.mk
@@ -8,6 +8,6 @@ KODI_PVR_PLUTOTV_VERSION = 22.3.0-Piers
 KODI_PVR_PLUTOTV_SITE = $(call github,kodi-pvr,pvr.plutotv,$(KODI_PVR_PLUTOTV_VERSION))
 KODI_PVR_PLUTOTV_LICENSE = GPL-2.0+
 KODI_PVR_PLUTOTV_LICENSE_FILES = LICENSE.md
-KODI_PVR_PLUTOTV_DEPENDENCIES = kodi rapidjson
+KODI_PVR_PLUTOTV_DEPENDENCIES = json-for-modern-cpp kodi
 
 $(eval $(cmake-package))
