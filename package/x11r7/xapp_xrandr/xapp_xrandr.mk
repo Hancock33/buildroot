dfee0bb8c6a51ea45c974665f2332cca1f24be4c
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-04-26 11:07:28 +0100

    update xapp_xrandr version 1.5.4

--- package/x11r7/xapp_xrandr/xapp_xrandr.mk
+++ package/x11r7/xapp_xrandr/xapp_xrandr.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XAPP_XRANDR_VERSION = 1.5.3
+XAPP_XRANDR_VERSION = 1.5.4
 XAPP_XRANDR_SOURCE = xrandr-$(XAPP_XRANDR_VERSION).tar.xz
 XAPP_XRANDR_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XRANDR_LICENSE = MIT
@@ -12,4 +12,4 @@ XAPP_XRANDR_LICENSE_FILES = COPYING
 XAPP_XRANDR_DEPENDENCIES = xlib_libXrandr xlib_libX11
 XAPP_XRANDR_CONF_OPTS = --disable-malloc0returnsnull
 
-$(eval $(autotools-package))
+$(eval $(meson-package))
