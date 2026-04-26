b8205caf7e0a220eeb63e430d7a3dd74fee7f32c
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-04-26 11:17:29 +0100

    update minizip version 4.1.2

--- package/minizip/minizip.mk
+++ package/minizip/minizip.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MINIZIP_VERSION = 4.1.1
+MINIZIP_VERSION = 4.1.2
 MINIZIP_SITE = $(call github,zlib-ng,minizip-ng,$(MINIZIP_VERSION))
 MINIZIP_DEPENDENCIES = host-pkgconf
 MINIZIP_INSTALL_STAGING = YES
