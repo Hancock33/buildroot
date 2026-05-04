77884f3ab6fc49129bb70099dc18029387157f14
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-05-04 20:20:43 +0100

    update poppler version 26.05.0

--- package/poppler/poppler.mk
+++ package/poppler/poppler.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-POPPLER_VERSION = 26.04.0
+POPPLER_VERSION = 26.05.0
 POPPLER_SOURCE = poppler-$(POPPLER_VERSION).tar.xz
 POPPLER_SITE = https://poppler.freedesktop.org
 POPPLER_DEPENDENCIES = fontconfig host-pkgconf
