354062ead42536a6f7428d9a2fae28b685aefe00
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-05-24 11:23:33 +0100

    update python-poetry-core version 2.4.1

--- package/python-poetry-core/python-poetry-core.mk
+++ package/python-poetry-core/python-poetry-core.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PYTHON_POETRY_CORE_VERSION = 2.4.0
+PYTHON_POETRY_CORE_VERSION = 2.4.1
 PYTHON_POETRY_CORE_SOURCE = poetry_core-$(PYTHON_POETRY_CORE_VERSION).tar.gz
-PYTHON_POETRY_CORE_SITE = https://files.pythonhosted.org/packages/b0/97/f7bb55470bb7890d9b3d3f9fa761083d5c9a6838b17c94a41bf2939f89ef
-PYTHON_POETRY_CORE_SETUP_TYPE = pep517
+PYTHON_POETRY_CORE_SITE = https://files.pythonhosted.org/packages/b2/f2/fa88c58efb9a737c7e0e40e470edd0973fe33f0f277d24dcf17454b50974
+PYTHON_POETRY_CORE_SETUP_TYPE = poetry
 PYTHON_POETRY_CORE_LICENSE = MIT
 PYTHON_POETRY_CORE_LICENSE_FILES = LICENSE
 
