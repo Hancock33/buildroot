12e586683fdf65b8b3ca051005e5afa5d8207b14
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-06-02 19:52:20 +0100

    update python-idna version 3.18

--- package/python-idna/python-idna.mk
+++ package/python-idna/python-idna.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_IDNA_VERSION = 3.17
+PYTHON_IDNA_VERSION = 3.18
 PYTHON_IDNA_SOURCE = idna-$(PYTHON_IDNA_VERSION).tar.gz
-PYTHON_IDNA_SITE = https://files.pythonhosted.org/packages/b9/28/99c51f664567218d824af024c0251650fb27e4ca066df188dab0769c5b91
+PYTHON_IDNA_SITE = https://files.pythonhosted.org/packages/cd/63/9496c57188a2ee585e0f1db071d75089a11e98aa86eb99d9d7618fc1edce
 PYTHON_IDNA_LICENSE = BSD-3-Clause
 PYTHON_IDNA_LICENSE_FILES = LICENSE.md
 PYTHON_IDNA_SETUP_TYPE = flit
