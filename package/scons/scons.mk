3caf3a8f9dedbbae5b3b7c07aca298e6b661587b
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-06-02 20:16:54 +0100

    update scons version 4.10.1

--- package/scons/scons.mk
+++ package/scons/scons.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-SCONS_VERSION = 4.9.1
+SCONS_VERSION = 4.10.1
 SCONS_SOURCE = scons-$(SCONS_VERSION).tar.gz
-SCONS_SITE = https://files.pythonhosted.org/packages/c8/c1/30176c76c1ef723fab62e5cdb15d3c972427a146cb6f868748613d7b25af
+SCONS_SITE = https://files.pythonhosted.org/packages/7d/c9/2f430bb39e4eccba32ce8008df4a3206df651276422204e177a09e12b30b
 SCONS_LICENSE = MIT
 SCONS_LICENSE_FILES = LICENSE
 SCONS_SETUP_TYPE = setuptools
