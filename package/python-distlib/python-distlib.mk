3508506b52ab41499da9b28627e907b80247d5b1
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-06-10 22:50:42 +0100

    update python-distlib version 0.4.2

--- package/python-distlib/python-distlib.mk
+++ package/python-distlib/python-distlib.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_DISTLIB_VERSION = 0.4.1
+PYTHON_DISTLIB_VERSION = 0.4.2
 PYTHON_DISTLIB_SOURCE = distlib-$(PYTHON_DISTLIB_VERSION).tar.gz
-PYTHON_DISTLIB_SITE = https://files.pythonhosted.org/packages/86/b2/d6fc3f2347f43dada79e5ff118493e8109c98400a0e29a1d5264a3aa479b
+PYTHON_DISTLIB_SITE = https://files.pythonhosted.org/packages/46/8d/873e9252ea2c0e0c857884e0a2899ec43ade132345df1925ef24cbe64f18
 PYTHON_DISTLIB_SETUP_TYPE = setuptools
 PYTHON_DISTLIB_LICENSE = PSF-2.0
 PYTHON_DISTLIB_LICENSE_FILES = LICENSE.txt
