eadd4e82ed84673414fd1f2a16fbd2d2b72301b6
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-06-23 21:30:20 +0100

    update python-vcs-versioning version 2.1.1

--- package/python-vcs-versioning/python-vcs-versioning.mk
+++ package/python-vcs-versioning/python-vcs-versioning.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-PYTHON_VCS_VERSIONING_VERSION = 1.1.1
+PYTHON_VCS_VERSIONING_VERSION = 2.1.1
 PYTHON_VCS_VERSIONING_SOURCE = vcs_versioning-$(PYTHON_VCS_VERSIONING_VERSION).tar.gz
-PYTHON_VCS_VERSIONING_SITE = https://files.pythonhosted.org/packages/49/42/d97a7795055677961c63a1eef8e7b19d5968ed992ed3a70ab8eb012efad8
+PYTHON_VCS_VERSIONING_SITE = https://files.pythonhosted.org/packages/d6/ba/b64191fdd9b7a42a976ee465050d09b859c4cef75a56951266c6ab6ea96a
 PYTHON_VCS_VERSIONING_SETUP_TYPE = setuptools
-PYTHON_VCS_VERSIONING_LICENSE = LGPL-3.0
+PYTHON_VCS_VERSIONING_LICENSE = MIT
+PYTHON_VCS_VERSIONING_LICENSE_FILES = LICENSE.txt
 
 $(eval $(host-python-package))
