a57ee87fff8572c68c9cf08df3c9132f2839725d
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-06-23 21:07:37 +0100

    Update python-numpy.mk

--- package/python-numpy/python-numpy.mk
+++ package/python-numpy/python-numpy.mk
@@ -98,18 +98,6 @@ HOST_PYTHON_NUMPY_NINJA_ENV += \
 # in-tree code.
 HOST_PYTHON_NUMPY_CONF_OPTS = -Dblas="" -Dlapack=""
 
-# Fixup the npymath.ini prefix path with actual target staging area where
-# numpy core was built. Without this, target builds using numpy distutils
-# extensions like python-scipy, python-numba cannot find -lnpymath since
-# it uses host libraries (like libnpymath.a).
-# So, the numpy distutils extension packages would explicitly link this
-# config path for their package environment.
-define PYTHON_NUMPY_FIXUP_NPY_PKG_CONFIG_FILES
-	$(SED) '/^pkgdir=/d;/^prefix=/i pkgdir=$(PYTHON3_PATH)/site-packages/numpy/core' \
-		$(PYTHON3_PATH)/site-packages/numpy/_core/lib/npy-pkg-config/npymath.ini
-endef
-PYTHON_NUMPY_POST_INSTALL_STAGING_HOOKS += PYTHON_NUMPY_FIXUP_NPY_PKG_CONFIG_FILES
-
 # Some package may include few headers from NumPy, so let's install it
 # in the staging area.
 PYTHON_NUMPY_INSTALL_STAGING = YES
