b38ba27d6c02e44a7f4e50cc74d0500c267783ee
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-07-01 20:57:01 +0100

    update python-setuptools-rust version 1.13.0

--- package/python-setuptools-rust/python-setuptools-rust.mk
+++ package/python-setuptools-rust/python-setuptools-rust.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_SETUPTOOLS_RUST_VERSION = 1.12.1
+PYTHON_SETUPTOOLS_RUST_VERSION = 1.13.0
 PYTHON_SETUPTOOLS_RUST_SOURCE = setuptools_rust-$(PYTHON_SETUPTOOLS_RUST_VERSION).tar.gz
-PYTHON_SETUPTOOLS_RUST_SITE = https://files.pythonhosted.org/packages/0c/11/736cfe4ec795dbce001ebdcef5cab03de1727041c805c3a45c1bda9f54a1
+PYTHON_SETUPTOOLS_RUST_SITE = https://files.pythonhosted.org/packages/68/ba/b31781d61bf9ee3c232a1d1160db11c11cdeae1d44e06c90723b25a8279f
 PYTHON_SETUPTOOLS_RUST_SETUP_TYPE = setuptools
 PYTHON_SETUPTOOLS_RUST_LICENSE = MIT
 PYTHON_SETUPTOOLS_RUST_LICENSE_FILES = LICENSE
