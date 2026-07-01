f31ebdfcc2b66ab50f796511da7b4680dc8ac66d
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-07-01 20:58:01 +0100

    update python-types-psutil version 7.2.2.20260518

--- package/python-types-psutil/python-types-psutil.mk
+++ package/python-types-psutil/python-types-psutil.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_TYPES_PSUTIL_VERSION = 7.2.2.20260130
+PYTHON_TYPES_PSUTIL_VERSION = 7.2.2.20260518
 PYTHON_TYPES_PSUTIL_SOURCE = types_psutil-$(PYTHON_TYPES_PSUTIL_VERSION).tar.gz
-PYTHON_TYPES_PSUTIL_SITE = https://files.pythonhosted.org/packages/69/14/fc5fb0a6ddfadf68c27e254a02ececd4d5c7fdb0efcb7e7e917a183497fb
+PYTHON_TYPES_PSUTIL_SITE = https://files.pythonhosted.org/packages/cb/f1/6901857281d4e8d792492e1495eef6f4f01318a3b6a066486d81000a4511
 PYTHON_TYPES_PSUTIL_SETUP_TYPE = setuptools
 PYTHON_TYPES_PSUTIL_LICENSE = Apache-2.0
 PYTHON_TYPES_PSUTIL_LICENSE_FILES = LICENSE
