4402f8160b604c1fbfbeea985f7586e1c0473606
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-07-10 18:04:58 +0100

    update xapp_xrdb version 1.2.3

--- package/x11r7/xapp_xrdb/xapp_xrdb.mk
+++ package/x11r7/xapp_xrdb/xapp_xrdb.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-XAPP_XRDB_VERSION = 1.2.2
+XAPP_XRDB_VERSION = 1.2.3
 XAPP_XRDB_SOURCE = xrdb-$(XAPP_XRDB_VERSION).tar.xz
 XAPP_XRDB_SITE = https://xorg.freedesktop.org/archive/individual/app
 XAPP_XRDB_LICENSE = MIT
 XAPP_XRDB_LICENSE_FILES = COPYING
 XAPP_XRDB_DEPENDENCIES = xlib_libX11 xlib_libXmu
 
-$(eval $(autotools-package))
+$(eval $(meson-package))
