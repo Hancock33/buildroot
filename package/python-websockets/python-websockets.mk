587edb39d9822e9ef10360f0b22e6050f7c2b560
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-07-21 20:09:03 +0100

    update python-websockets version 16.1.1

--- package/python-websockets/python-websockets.mk
+++ package/python-websockets/python-websockets.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_WEBSOCKETS_VERSION = 16.1
+PYTHON_WEBSOCKETS_VERSION = 16.1.1
 PYTHON_WEBSOCKETS_SOURCE = websockets-$(PYTHON_WEBSOCKETS_VERSION).tar.gz
-PYTHON_WEBSOCKETS_SITE = https://files.pythonhosted.org/packages/8c/02/b9a097e1e16fee4e2fd1ec8c39f6a9c5d6257bae8fa12640caf869f54436
+PYTHON_WEBSOCKETS_SITE = https://files.pythonhosted.org/packages/21/f7/bc3a25c5ec26ce62ce487690becc2f3710bbc7b33338f005ad390db0b986
 PYTHON_WEBSOCKETS_SETUP_TYPE = setuptools
 PYTHON_WEBSOCKETS_LICENSE = BSD-3-Clause
 PYTHON_WEBSOCKETS_LICENSE_FILES = LICENSE
