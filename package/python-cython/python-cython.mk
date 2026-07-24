bc1684a3750a291c2533ff2ca0c2f2e82a58a299
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-07-24 17:01:19 +0100

    update python-cython version 3.2.9

--- package/python-cython/python-cython.mk
+++ package/python-cython/python-cython.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_CYTHON_VERSION = 3.2.8
+PYTHON_CYTHON_VERSION = 3.2.9
 PYTHON_CYTHON_SOURCE = cython-$(PYTHON_CYTHON_VERSION).tar.gz
-PYTHON_CYTHON_SITE = https://files.pythonhosted.org/packages/b6/6b/80101e02ebacaf9232ecf32bf6a788d36b27d820ee02434746252569ef98
+PYTHON_CYTHON_SITE = https://files.pythonhosted.org/packages/f6/de/db48b8870e766cfea809986cc50c1e986c663a9ab7bafd0ac1a2512c4a26
 PYTHON_CYTHON_SETUP_TYPE = setuptools
 PYTHON_CYTHON_LICENSE = Apache-2.0
 PYTHON_CYTHON_LICENSE_FILES = COPYING.txt LICENSE.txt
