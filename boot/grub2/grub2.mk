1bd757b1ca4b853e965e16a5a9505b8d1df23b85
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-08-08 20:17:58 +0100

    update grub autoreconf

--- boot/grub2/grub2.mk
+++ boot/grub2/grub2.mk
@@ -15,11 +15,7 @@ HOST_GRUB2_DEPENDENCIES = host-bison host-flex host-gawk \
 	$(BR2_PYTHON3_HOST_DEPENDENCY)
 GRUB2_INSTALL_IMAGES = YES
 
-# 0001-Revert-configure-Check-linker-for-image-base-support.patch
-# 0002-Revert-configure-Print-a-more-helpful-error-if-autoc.patch
-GRUB2_AUTORECONF = YES
 GRUB2_DEPENDENCIES += host-pkgconf
-HOST_GRUB2_AUTORECONF = YES
 HOST_GRUB2_DEPENDENCIES += host-pkgconf
 
 ifeq ($(BR2_TARGET_GRUB2_INSTALL_TOOLS),y)
@@ -197,6 +193,12 @@ define GRUB2_INSTALL_TARGET_CMDS
 	)
 endef
 endif
+
+define GRUB2_AUTOCONFIG
+	cd $(@D) && PATH=/usr/bin autoreconf -f -i
+endef
+GRUB2_PRE_CONFIGURE_HOOKS += GRUB2_AUTOCONFIG
+HOST_GRUB2_PRE_CONFIGURE_HOOKS += GRUB2_AUTOCONFIG
 
 $(eval $(generic-package))
 $(eval $(host-autotools-package))
