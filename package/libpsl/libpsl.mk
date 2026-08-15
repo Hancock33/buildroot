904fb6b9d38fabec51ac3c12211f0995a8c86e99
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-08-15 20:49:21 +0100

    update libpsl version 0.23.3

--- package/libpsl/libpsl.mk
+++ package/libpsl/libpsl.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBPSL_VERSION = 0.23.2
+LIBPSL_VERSION = 0.23.3
 LIBPSL_SITE = https://github.com/rockdaboot/libpsl/releases/download/$(LIBPSL_VERSION)
 LIBPSL_LICENSE = MIT, BSD-3-Clause
 LIBPSL_LICENSE_FILES = COPYING src/LICENSE.chromium
