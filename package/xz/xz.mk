889d5d23db957d6e0ff3854da3087d81d46f2fe5
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-09-10 20:16:38 +0100

    update xz version 5.8.4

--- package/xz/xz.mk
+++ package/xz/xz.mk
@@ -4,11 +4,10 @@
 #
 ################################################################################
 
-XZ_VERSION = 5.8.3
+XZ_VERSION = 5.8.4
 XZ_SOURCE = xz-$(XZ_VERSION).tar.bz2
 XZ_SITE = https://github.com/tukaani-project/xz/releases/download/v$(XZ_VERSION)
 XZ_INSTALL_STAGING = YES
-XZ_CONF_ENV = ac_cv_prog_cc_c99='-std=gnu99'
 XZ_LICENSE = 0BSD, GPL-2.0+, GPL-3.0+, LGPL-2.1+, FSFUL, FSFULLR, Public Domain
 XZ_LICENSE_FILES = COPYING COPYING.0BSD COPYING.GPLv2 COPYING.GPLv3 COPYING.LGPLv2.1
 XZ_CPE_ID_VENDOR = tukaani
@@ -17,63 +16,53 @@ XZ_CPE_ID_VENDOR = tukaani
 HOST_XZ_ADD_CCACHE_DEPENDENCY = NO
 
 XZ_CONF_OPTS = \
-	--enable-encoders=lzma1,lzma2,delta,x86,powerpc,ia64,arm,armthumb,arm64,sparc,riscv \
-	--enable-decoders=lzma1,lzma2,delta,x86,powerpc,ia64,arm,armthumb,arm64,sparc,riscv \
-	--enable-match-finders=hc3,hc4,bt2,bt3,bt4 \
-	--enable-checks=crc32,crc64,sha256 \
-	--disable-external-sha256 \
-	--enable-microlzma \
-	--enable-lzip-decoder \
-	--enable-assembler \
-	--enable-clmul-crc \
-	--enable-arm64-crc32 \
-	--disable-small \
-	--enable-assume-ram=128 \
-	--enable-xz \
-	--enable-xzdec \
-	--enable-lzmadec \
-	--enable-lzmainfo \
-	--enable-lzma-links \
-	--enable-scripts \
-	--enable-sandbox=auto \
-	--enable-symbol-versions \
-	--enable-rpath \
-	--enable-largefile \
-	--enable-unaligned-access=auto \
-	--disable-unsafe-type-punning \
-	--disable-werror \
-	--enable-year2038
+	-DXZ_ENCODERS="lzma1;lzma2;delta;x86;powerpc;ia64;arm;armthumb;arm64;sparc;riscv" \
+	-DXZ_DECODERS="lzma1;lzma2;delta;x86;powerpc;ia64;arm;armthumb;arm64;sparc;riscv" \
+	-DXZ_MATCH_FINDERS="hc3;hc4;bt2;bt3;bt4" \
+	-DXZ_CHECKS="crc32;crc64;sha256" \
+	-DXZ_EXTERNAL_SHA256=FALSE \
+	-DXZ_MICROLZMA_DECODER=TRUE \
+	-DXZ_MICROLZMA_ENCODER=TRUE \
+	-DXZ_LZIP_DECODER=TRUE \
+	-DXZ_CLMUL_CRC=TRUE \
+	-DXZ_ARM64_CRC32=TRUE \
+	-DXZ_SMALL=FALSE \
+	-DXZ_ASSUME_RAM=128 \
+	-DXZ_TOOL_XZ=TRUE \
+	-DXZ_TOOL_XZDEC=TRUE \
+	-DXZ_TOOL_LZMADEC=TRUE \
+	-DXZ_TOOL_LZMAINFO=TRUE \
+	-DXZ_TOOL_SYMLINKS_LZMA=TRUE \
+	-DXZ_TOOL_SCRIPTS=TRUE \
+	-DXZ_SANDBOX=auto \
+	-DXZ_SYMBOL_VERSIONING=yes \
+	-DTUKLIB_FAST_UNALIGNED_ACCESS=auto \
+	-DTUKLIB_USE_UNSAFE_TYPE_PUNNING=FALSE \
+	-DXZ_DOC=FALSE
 
 ifeq ($(BR2_SYSTEM_ENABLE_NLS),y)
-XZ_CONF_OPTS += --enable-nls
+XZ_CONF_OPTS += -DXZ_NLS=TRUE
 else
-XZ_CONF_OPTS += --disable-nls
+XZ_CONF_OPTS += -DXZ_NLS=FALSE
 endif
 
 ifeq ($(BR2_TOOLCHAIN_HAS_THREADS),y)
-XZ_CONF_OPTS += --enable-threads
+XZ_CONF_OPTS += -DXZ_THREADS=yes
 else
-XZ_CONF_OPTS += --disable-threads
+XZ_CONF_OPTS += -DXZ_THREADS=no
 endif
 
 HOST_XZ_CONF_OPTS = \
 	$(XZ_CONF_OPTS) \
-	--enable-nls \
-	--enable-threads
+	-DCMAKE_C_COMPILER_LAUNCHER=""
+	-DCMAKE_CXX_COMPILER_LAUNCHER=""
+	-DXZ_NLS=TRUE \
+	-DXZ_THREADS=TRUE
 
 # we are built before ccache
 HOST_XZ_CONF_ENV = \
 	CC="$(HOSTCC_NOCCACHE)" \
 	CXX="$(HOSTCXX_NOCCACHE)"
 
-# We need to prevent XZ_AUTORECONF for host builds or we end up with a
-# circular dependency. Since the autoconf build needs to extract a
-# tar.xz archive, autoconf has an implicit dependency on HOST_XZ. By
-# enabling XZ_AUTORECONF we also make host-xz depend on autoconf,
-# which we can't do. It is also not necessary as we're autoreconfuring
-# the target package to fix static build with musl, which is
-# irrelevant for the host package.
-HOST_XZ_AUTORECONF = NO
-
-$(eval $(autotools-package))
-$(eval $(host-autotools-package))
+$(eval $(cmake-package))
+$(eval $(host-cmake-package))
