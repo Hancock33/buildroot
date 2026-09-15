0b2ac0eb3b5278ed131fc9f9efd359877825d58a
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-09-15 22:14:21 +0100

    buildtools

--- package/mesa3d/mesa3d.mk
+++ package/mesa3d/mesa3d.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # git describe --abbrev=40 origin/staging/26.0 | cut -d '-' -f 2-
-MESA3D_VERSION = 26.2.2-1-g0ae52750c0ccc84fda91b5a45708325b189073c7
+MESA3D_VERSION = 26.2.2-72-gbefa75a64f9665ebeb1c0d454790d6c4bddc73b8
 MESA3D_SITE = $(call gitlabfreedesktop,mesa,mesa,$(MESA3D_VERSION))
 
 MESA3D_LICENSE = MIT, SGI, Khronos
--- package/vlc/vlc.mk
+++ package/vlc/vlc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-VLC_VERSION = 3.0.23-2-1260-ga722106c163000807c43188274b2deb6b64518a9
+VLC_VERSION = 3.0.23-2-1271-gbb629d71226008b8e8cdbf91fe6caa85b3f01694
 VLC_SITE = https://code.videolan.org/videolan/vlc.git
 VLC_SITE_METHOD = git
 #VLC_SOURCE = vlc-$(VLC_VERSION).tar.xz
