8d3b205313aac890f60e422e7e0a5c920cd5344e
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-09-16 20:19:22 +0100

    update python-propcache version 0.5.4

--- package/python-propcache/python-propcache.mk
+++ package/python-propcache/python-propcache.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PROPCACHE_VERSION = 0.5.2
+PYTHON_PROPCACHE_VERSION = 0.5.4
 PYTHON_PROPCACHE_SOURCE = propcache-$(PYTHON_PROPCACHE_VERSION).tar.gz
-PYTHON_PROPCACHE_SITE = https://files.pythonhosted.org/packages/ec/44/c87281c333769159c50594f22610f77398a47ccbfbbf23074e744e86f87c
+PYTHON_PROPCACHE_SITE = https://files.pythonhosted.org/packages/b3/9a/9fbf4e4ec0c2d7f1c32519fff782ef467859b8faa9fbc5331a96f6395d43
 PYTHON_PROPCACHE_SETUP_TYPE = setuptools
 PYTHON_PROPCACHE_LICENSE = Apache-2.0
 PYTHON_PROPCACHE_LICENSE_FILES = LICENSE
