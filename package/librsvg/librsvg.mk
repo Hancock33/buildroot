6ddf2cda0939b8fc6775bd411db17329050211aa
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-09-20 22:23:35 +0100

    update librsvg version 2.63.2

--- package/librsvg/librsvg.mk
+++ package/librsvg/librsvg.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBRSVG_VERSION = 2.63.0
+LIBRSVG_VERSION = 2.63.2
 LIBRSVG_SITE = https://gitlab.gnome.org/GNOME/librsvg/-/archive/$(LIBRSVG_VERSION)
 LIBRSVG_INSTALL_STAGING = YES
 LIBRSVG_LICENSE = LGPL-2.1+
