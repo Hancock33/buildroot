eb8d60b116016eb0a42a156544db6a6b48f1cede
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2026-09-25 07:27:25 +0100

    update cairo version 1.18.6

--- package/cairo/cairo.mk
+++ package/cairo/cairo.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CAIRO_VERSION = 1.18.4
+CAIRO_VERSION = 1.18.6
 CAIRO_SOURCE = cairo-$(CAIRO_VERSION).tar.xz
 CAIRO_LICENSE = LGPL-2.1 or MPL-1.1 (library)
 CAIRO_LICENSE_FILES = COPYING COPYING-LGPL-2.1 COPYING-MPL-1.1
